3b58131c6c8fae8ec6b5e971866eb491147656c8
Grégory Le Garec <gregory.legarec@gmail.com>
2018-07-06 09:52:32 +0200

    fix: 📝  Fix AppCollection

--- packages/cozy-stack-client/src/AppCollection.js
+++ packages/cozy-stack-client/src/AppCollection.js
@@ -1,19 +1,19 @@
-import { uri } from './utils'
 import { normalizeDoc } from './DocumentCollection'
+import { uri } from './utils'
 
-export const APP_DOCTYPE = 'io.cozy.apps'
+export const APPS_DOCTYPE = 'io.cozy.apps'
 
-/**
- * Abstracts a collection of documents of the same doctype, providing
- * CRUD methods and other helpers.
- */
+export const normalizeApp = app => {
+  return { ...app, ...normalizeDoc(app, APPS_DOCTYPE), ...app.attributes }
+}
+
 export default class AppCollection {
   constructor(client) {
     this.client = client
   }
 
   /**
-   * Lists all documents of the collection, without filters.
+   * Lists all apps, without filters.
    *
    * The returned documents are not paginated by the stack.
    *
@@ -24,8 +24,10 @@ export default class AppCollection {
     const path = uri`/apps/`
     const resp = await this.client.fetchJSON('GET', path)
     return {
-      data: resp.data.map(row => normalizeDoc(row, APP_DOCTYPE)),
-      meta: { count: resp.total_rows },
+      data: resp.data.map(app => normalizeApp(app)),
+      meta: {
+        count: resp.meta.count
+      },
       skip: 0,
       next: false
     }
