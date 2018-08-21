03657ec94a3cd56165aad878aced6cefff70d825
Patrick Browne <pt.browne@gmail.com>
2018-08-21 15:45:04 +0200

    fix: when watching a query, ensure it exists in store

--- packages/cozy-client/src/CozyClient.js
+++ packages/cozy-client/src/CozyClient.js
@@ -196,6 +196,7 @@ export default class CozyClient {
 
   watchQuery(queryDefinition, options = {}) {
     const queryId = options.as || this.generateId()
+    this.ensureQueryExists(queryId, queryDefinition)
     return new ObservableQuery(queryId, queryDefinition, this)
   }
 
