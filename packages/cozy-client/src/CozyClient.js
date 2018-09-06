6a3a07331432cb873e4b73c282abc4531b1e70c2
Patrick Browne <pt.browne@gmail.com>
2018-09-06 10:23:15 +0200

    fixup: Unintented change

--- packages/cozy-client/src/CozyClient.js
+++ packages/cozy-client/src/CozyClient.js
@@ -274,7 +274,7 @@ export default class CozyClient {
     this.ensureStore()
     const existingQuery = getQueryFromState(this.store.getState(), queryId)
     // Don't trigger the INIT_QUERY for fetchMore() calls
-    if (existingQuery.fetchStatus !== 'loaded') {
+    if (existingQuery.fetchStatus !== 'loaded' || !queryDefinition.skip) {
       this.dispatch(initQuery(queryId, queryDefinition))
     }
   }
