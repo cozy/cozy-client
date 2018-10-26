5f89372bbbb17bccf558bec0fb2ab0237ddb9b48
Patrick Browne <pt.browne@gmail.com>
2018-10-26 18:07:04 +0200

    chore: Add example

---
+++ packages/cozy-client/examples/file-relationships.js
@@ -0,0 +1,44 @@
+const minimist = require('minimist')
+const { QueryDefinition, default: CozyClient } = require('../dist')
+global.fetch = require('node-fetch') // in the browser we have native fetch
+
+const schema = {
+  albums: {
+    doctype: 'io.cozy.photos.albums',
+    attributes: {
+      name: {
+        type: 'string',
+        unique: true
+      }
+    },
+    relationships: {
+      photos: {
+        type: 'has-many',
+        doctype: 'io.cozy.files'
+      }
+    }
+  }
+}
+
+const main = async _args => {
+  const uri = 'https://' + process.env.COZY_URL || 'http://cozy.tools:8080'
+  const token = process.env.COZY_TOKEN
+  if (!token) {
+    throw new Error('You should provide COZY_TOKEN as an environement variable')
+  }
+  const args = minimist(_args.slice(2), {
+    string: ['selector']
+  })
+  const client = new CozyClient({ uri, token, schema })
+  const query = new QueryDefinition({
+    doctype: 'io.cozy.photos.albums',
+    limit: 10
+  }).include(['photos'])
+
+  const resp = await client.query(query)
+  const albums = client.hydrateDocuments('io.cozy.photos.albums', resp.data)
+  console.log(albums[0].name)
+  console.log(albums[0].photos.data)
+}
+
+main(process.argv).catch(e => console.error(e))
