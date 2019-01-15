02ecf4389ab12b5c3b3c92a2cdb2d25425e6290a
Simon Constans <simonc@linux.com>
2019-01-15 12:21:35 +0100

    feat: Add urls helpers

---
+++ packages/cozy-stack-client/src/urls.spec.js
@@ -0,0 +1,41 @@
+import {
+  isSecureProtocol,
+  getNodeCozyURL,
+  getBrowserCozyURL,
+  getCozyURL
+} from './urls'
+
+const cozyDomain = 'cozy.tools:8080'
+const fullCozyUrlNoSSL = 'http://cozy.tools:8080'
+const fullCozyUrlWithSSL = 'https://cozy.tools:8080'
+const prodUrl = 'https://prod.mycozy.cloud'
+
+describe('urls', () => {
+  it('should return browser cozy url', () => {
+    expect(() => getBrowserCozyURL()).toThrowErrorMatchingSnapshot()
+    document.querySelector = jest
+      .fn()
+      .mockImplementation(() => ({ dataset: { cozyDomain } }))
+    window.location.protocol = 'http:'
+    expect(getBrowserCozyURL().origin).toBe(fullCozyUrlNoSSL)
+    document.querySelector.mockReset()
+  })
+
+  it('should return node cozy url', () => {
+    process.env.COZY_URL = undefined
+    expect(() => getNodeCozyURL()).toThrowErrorMatchingSnapshot()
+    process.env.COZY_URL = fullCozyUrlNoSSL
+    expect(getNodeCozyURL().origin).toBe(fullCozyUrlNoSSL)
+  })
+
+  it('should return cozy url', () => {
+    expect(getCozyURL().origin).toBe(fullCozyUrlNoSSL)
+  })
+
+  it('should return if secure protocol is used', () => {
+    expect(isSecureProtocol()).toBe(false)
+    expect(isSecureProtocol(new URL(fullCozyUrlNoSSL))).toBe(false)
+    expect(isSecureProtocol(new URL(fullCozyUrlWithSSL))).toBe(true)
+    expect(isSecureProtocol(new URL(prodUrl))).toBe(true)
+  })
+})
