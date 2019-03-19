a5eb7e8f7c0a2d67af87496450e414beea15ecba
Grégory Le Garec <gregory.legarec@gmail.com>
2019-03-19 15:44:41 +0100

    fix(stack): Make KonnectorCollection export KONNECTORS_DOCTYPE 📝

--- packages/cozy-stack-client/src/KonnectorCollection.js
+++ packages/cozy-stack-client/src/KonnectorCollection.js
@@ -1,5 +1,7 @@
 import AppCollection from './AppCollection'
 
+export const KONNECTORS_DOCTYPE = 'io.cozy.konnectors'
+
 class KonnectorCollection extends AppCollection {
   endpoint = '/konnectors/'
 }
