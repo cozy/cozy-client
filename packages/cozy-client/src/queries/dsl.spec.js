eb6356cd91e6efe9032fe80ce3999a2a22f62cfe
Gara64 <paul@cozycloud.cc>
2019-05-31 16:10:39 +0200

    test: Get documents on ids
    test: Fix test

--- packages/cozy-client/src/queries/dsl.spec.js
+++ packages/cozy-client/src/queries/dsl.spec.js
@@ -1,11 +1,27 @@
 import { QueryDefinition as Q } from '../queries/dsl'
 
 describe('QueryDefinition', () => {
-  it('should build query defs', () => {
+  it('should build query defs on selected fields', () => {
     const q = new Q({ doctype: 'io.cozy.todos' })
     expect(q.select(['toto'])).toMatchObject({
       doctype: 'io.cozy.todos',
       fields: ['toto']
+    })
+  })
+
+  it('should build query def on id', () => {
+    const q = new Q({ doctype: 'io.cozy.todos' })
+    expect(q.getById('id1')).toMatchObject({
+      id: 'id1',
+      doctype: 'io.cozy.todos'
+    })
+  })
+
+  it('should build query def on ids', () => {
+    const q = new Q({ doctype: 'io.cozy.todos' })
+    expect(q.getByIds(['id1', 'ids2'])).toMatchObject({
+      ids: ['id1', 'ids2'],
+      doctype: 'io.cozy.todos'
     })
   })
 })
