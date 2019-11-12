64f19240319313410d1cfb8a759776f6b67f4854
Patrick Browne <pt.browne@gmail.com>
2019-11-12 09:03:24 +0100

    feat: Expose Q helper to build query definitions

--- packages/cozy-client/src/queries/dsl.spec.js
+++ packages/cozy-client/src/queries/dsl.spec.js
@@ -1,8 +1,8 @@
-import { QueryDefinition as Q } from '../queries/dsl'
+import { QueryDefinition, Q } from '../queries/dsl'
 
 describe('QueryDefinition', () => {
   it('should build query defs on selected fields', () => {
-    const q = new Q({ doctype: 'io.cozy.todos' })
+    const q = new QueryDefinition({ doctype: 'io.cozy.todos' })
     expect(q.select(['toto'])).toMatchObject({
       doctype: 'io.cozy.todos',
       fields: ['toto']
@@ -10,7 +10,7 @@ describe('QueryDefinition', () => {
   })
 
   it('should build query def on id', () => {
-    const q = new Q({ doctype: 'io.cozy.todos' })
+    const q = new QueryDefinition({ doctype: 'io.cozy.todos' })
     expect(q.getById('id1')).toMatchObject({
       id: 'id1',
       doctype: 'io.cozy.todos'
@@ -18,10 +18,18 @@ describe('QueryDefinition', () => {
   })
 
   it('should build query def on ids', () => {
-    const q = new Q({ doctype: 'io.cozy.todos' })
+    const q = new QueryDefinition({ doctype: 'io.cozy.todos' })
     expect(q.getByIds(['id1', 'ids2'])).toMatchObject({
       ids: ['id1', 'ids2'],
       doctype: 'io.cozy.todos'
+    })
+  })
+
+  it('should work with shorthand Q', () => {
+    const q = Q('io.cozy.files')
+    expect(q.getByIds(['id1', 'ids2'])).toMatchObject({
+      ids: ['id1', 'ids2'],
+      doctype: 'io.cozy.files'
     })
   })
 })
