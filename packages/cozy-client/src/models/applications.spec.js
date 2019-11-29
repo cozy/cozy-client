7662719e08496a4b679be16108c3d755e7a5e216
Yannick Lohse <contact@yannick-lohse.fr>
2019-11-29 09:57:39 +0100

    App model (#575)
    * feat: Migrated app model
    
    * feat: Added app display name
    
    * Update packages/cozy-client/src/models/applications.js
    
    Co-Authored-By: Patrick Browne <pt.browne@gmail.com>
    
    * Update packages/cozy-client/src/models/applications.js
    
    Co-Authored-By: Patrick Browne <pt.browne@gmail.com>
    
    * Update packages/cozy-client/src/models/applications.js
    
    Co-Authored-By: Patrick Browne <pt.browne@gmail.com>
    
    * Update packages/cozy-client/src/models/applications.js
    
    Co-Authored-By: Patrick Browne <pt.browne@gmail.com>
    
    * docs: Added method description
    
    * docs: Regenerate docs

---
+++ packages/cozy-client/src/models/applications.spec.js
@@ -0,0 +1,83 @@
+import { getAppDisplayName } from './applications'
+
+describe('applications model', () => {
+  describe('application name', () => {
+    it('should return names', () => {
+      expect(getAppDisplayName({ name_prefix: 'Test', name: 'App' })).toBe(
+        'Test App'
+      )
+      expect(getAppDisplayName({ name_prefix: '', name: 'App' })).toBe('App')
+      expect(getAppDisplayName({ name: 'App' })).toBe('App')
+      expect(getAppDisplayName({ name_prefix: '', name: 'My App' })).toBe(
+        'My App'
+      )
+      expect(getAppDisplayName({ name_prefix: 'Cozy', name: 'Cloud' })).toBe(
+        'Cloud'
+      )
+      expect(getAppDisplayName({ name_prefix: 'Cozy', name: 'Cozy' })).toBe(
+        'Cozy'
+      )
+    })
+
+    it('should support translations', () => {
+      const lang = 'fr'
+      expect(
+        getAppDisplayName(
+          {
+            name_prefix: 'Test',
+            name: 'One',
+            locales: {
+              fr: {
+                name_prefix: 'Essai',
+                name: 'Un'
+              }
+            }
+          },
+          lang
+        )
+      ).toBe('Essai Un')
+      expect(
+        getAppDisplayName(
+          {
+            name_prefix: 'Test',
+            name: 'One',
+            locales: {
+              fr: {
+                name: 'Un'
+              }
+            }
+          },
+          lang
+        )
+      ).toBe('Test Un')
+      expect(
+        getAppDisplayName(
+          {
+            name_prefix: 'Test',
+            name: 'One',
+            locales: {
+              fr: {
+                name_prefix: 'Essai'
+              }
+            }
+          },
+          lang
+        )
+      ).toBe('Essai One')
+      expect(
+        getAppDisplayName(
+          {
+            name_prefix: 'Test',
+            name: 'One',
+            locales: {
+              de: {
+                name_prefix: 'Versuch'
+              }
+            }
+          },
+          lang
+        )
+      ).toBe('Test One')
+    })
+  })
+})
