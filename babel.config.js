3de520a588126f43708a97efafdb75f65ec8ae49
Patrick Browne <pt.browne@gmail.com>
2019-12-06 10:42:10 +0100

    fix: Require regenerator-runtime from @babel/runtime/regenerator
    At some point cozy-client was shipped on npm with regeneratorRuntime
    not being correctly imported. It must be because in 1e53a167fa43117d1027423b814f9000f427da87,
    the plugin transform-runtime was no longer imported, and did not override
    the transformRegenerator: false specified in the line just above. As a
    lib, cozy-client should not make any assumption on its environment, ie
    it should not take regeneratorRuntime from global/root

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,10 @@
 const pacakgeJson = require('./packages/cozy-client/package.json')
 
 module.exports = function(api) {
-  const isTest = api.env('test')
   api.cache(true)
 
   return {
-    presets: [
-      [
-        'cozy-app',
-        {
-          transformRuntime: {
-            regenerator: isTest
-          }
-        }
-      ]
-    ],
+    presets: [['cozy-app']],
     plugins: [
       [
         'search-and-replace',
