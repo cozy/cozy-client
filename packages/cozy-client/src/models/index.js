bed04457cbe95d76055d7b42408291ee10390b52
Yannick Lohse <contact@yannick-lohse.fr>
2019-12-16 16:30:59 +0100

    feat: Get accounts last error

--- packages/cozy-client/src/models/index.js
+++ packages/cozy-client/src/models/index.js
@@ -2,6 +2,7 @@ import * as triggers from './triggers'
 import * as instance from './instance'
 import * as applications from './applications'
 import * as file from './file'
+import * as accounts from './accounts'
 import * as note from './note'
 
-export { triggers, instance, applications, file, note }
+export { triggers, instance, applications, file, note, accounts }
