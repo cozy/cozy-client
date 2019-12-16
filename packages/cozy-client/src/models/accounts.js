8d0ce97f973c9336fc1369b39bd6c3f5f5aa5eae
Yannick Lohse <contact@yannick-lohse.fr>
2019-12-16 16:45:39 +0100

    feat: Mute account errors

--- packages/cozy-client/src/models/accounts.js
+++ packages/cozy-client/src/models/accounts.js
@@ -8,3 +8,21 @@ import get from 'lodash/get'
  * @returns {Array} AN array of errors with a `type` and `mutedAt` field
  */
 export const getMutedErrors = account => get(account, 'mutedErrors', [])
+
+/**
+ * muteError - Adds an error to the list of muted errors for the given account
+ *
+ * @param {object} account   io.cozy.accounts
+ * @param {string} errorType The type of the error to mute
+ *
+ * @returns {object} An updated io.cozy.accounts
+ */
+export const muteError = (account, errorType) => {
+  const mutedErrors = getMutedErrors(account)
+  mutedErrors.push({
+    type: errorType,
+    mutedAt: new Date().toISOString()
+  })
+
+  return { ...account, mutedErrors }
+}
--- packages/cozy-client/src/models/accounts.spec.js
+++ packages/cozy-client/src/models/accounts.spec.js
@@ -1,7 +1,19 @@
+import MockDate from 'mockdate'
 import { accounts } from './'
-const { getMutedErrors } = accounts
+const { getMutedErrors, muteError } = accounts
 
 describe('accounts model', () => {
+  const MOCKED_DATE = '2018-05-05T09:09:00.115Z'
+
+  beforeAll(() => {
+    MockDate.set(MOCKED_DATE)
+  })
+
+  afterAll(() => {
+    jest.restoreAllMocks()
+    MockDate.reset()
+  })
+
   it('should return the listy of muted errors', () => {
     const accountWithMutedErrors = {
       account_type: 'example-konnector',
@@ -37,5 +49,53 @@ describe('accounts model', () => {
       }
     }
     expect(getMutedErrors(accountWithoutMutedErrors)).toEqual([])
+  })
+
+  it('should mute the first error', () => {
+    const baseAccount = {
+      account_type: 'example-konnector'
+    }
+    expect(muteError(baseAccount, 'LOGIN_FAILED')).toEqual({
+      account_type: 'example-konnector',
+      mutedErrors: [
+        {
+          type: 'LOGIN_FAILED',
+          mutedAt: MOCKED_DATE
+        }
+      ]
+    })
+  })
+
+  it('should mute subsequent error', () => {
+    const account = {
+      account_type: 'example-konnector',
+      mutedErrors: [
+        {
+          type: 'LOGIN_FAILED',
+          mutedAt: '2018-04-05T09:09:00.115Z'
+        },
+        {
+          type: 'USER_ACTION_NEEDED',
+          mutedAt: '2018-04-15T09:09:00.115Z'
+        }
+      ]
+    }
+    expect(muteError(account, 'LOGIN_FAILED')).toEqual({
+      account_type: 'example-konnector',
+      mutedErrors: [
+        {
+          type: 'LOGIN_FAILED',
+          mutedAt: '2018-04-05T09:09:00.115Z'
+        },
+        {
+          type: 'USER_ACTION_NEEDED',
+          mutedAt: '2018-04-15T09:09:00.115Z'
+        },
+        {
+          type: 'LOGIN_FAILED',
+          mutedAt: MOCKED_DATE
+        }
+      ]
+    })
   })
 })
