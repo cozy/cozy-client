3391de7071fbb6f7de2c6b839749813120fd7081
Éric D <15271+edas@users.noreply.github.com>
2019-12-18 15:25:26 +0100

    feat: Share a link with special permissions
    This will be useful for cozy-notes. We want links
    to accept PATCH and PUT so the recipient can
    edit the note.

--- packages/cozy-stack-client/src/PermissionCollection.js
+++ packages/cozy-stack-client/src/PermissionCollection.js
@@ -104,7 +104,15 @@ class PermissionCollection extends DocumentCollection {
     return { ...resp, data: resp.data.map(a => ({ _id: a.id, ...a })) }
   }
 
-  async createSharingLink(document) {
+  /**
+   * Create a share link
+   *
+   * @param {{_id, _type}} document - cozy document
+   * @param {object} options - options
+   * @param {string[]} options.verbs - explicit permissions to use
+   */
+  async createSharingLink(document, options = {}) {
+    const { verbs } = options
     const resp = await this.stackClient.fetchJSON(
       'POST',
       `/permissions?codes=email`,
@@ -112,7 +120,7 @@ class PermissionCollection extends DocumentCollection {
         data: {
           type: 'io.cozy.permissions',
           attributes: {
-            permissions: getPermissionsFor(document, true)
+            permissions: getPermissionsFor(document, verbs ? { verbs } : true)
           }
         }
       }
@@ -143,9 +151,18 @@ class PermissionCollection extends DocumentCollection {
   }
 }
 
-const getPermissionsFor = (document, publicLink = false) => {
+/**
+ * Build a permission set
+ *
+ * @param {{_id, _type}} document - cozy document
+ * @param {boolean} publicLink - are the permissions for a public link ?
+ * @param {object} options - options
+ * @param {string[]} options.verbs - explicit permissions to use
+ * @returns {object} permissions object that can be sent through /permissions/*
+ */
+const getPermissionsFor = (document, publicLink = false, options = {}) => {
   const { _id, _type } = document
-  const verbs = publicLink ? ['GET'] : ['ALL']
+  const verbs = options.verbs ? verbs : publicLink ? ['GET'] : ['ALL']
   // TODO: this works for albums, but it needs to be generalized and integrated
   // with cozy-client ; some sort of doctype "schema" will be needed here
   return isFile(document)
