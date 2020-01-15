030eb79230662818496eab8a190d1911dec5c2ca
Quentin Valmori <quentin.valmori@gmail.com>
2020-01-15 11:12:48 +0100

    test: Add test for error

--- packages/cozy-stack-client/src/memoize.spec.js
+++ packages/cozy-stack-client/src/memoize.spec.js
@@ -1,7 +1,8 @@
-import memoize from './memoize'
+import memoize, { ErrorReturned } from './memoize'
 
 describe('memoize', () => {
   let now
+  let c
   beforeEach(() => {
     jest.spyOn(Date, 'now').mockImplementation(() => now)
   })
@@ -10,7 +11,7 @@ describe('memoize', () => {
   })
   it('should remember results ', () => {
     now = 0
-    let c = 0
+    c = 0
     const counter = () => c++
     const mcounter = memoize(counter, { key: () => 'A', maxDuration: 5 })
     expect(mcounter()).toBe(0)
@@ -18,5 +19,24 @@ describe('memoize', () => {
     now = 6
     expect(mcounter()).toBe(1)
     expect(mcounter()).toBe(1)
+  })
+
+  it('should not memoize the result if an error occurs', async () => {
+    now = 0
+    c = 0
+    const counter = async () => {
+      c++
+      return new Promise(function(resolve, reject) {
+        resolve(new ErrorReturned())
+      })
+    }
+    const mcounter = memoize(counter, { key: () => 'A', maxDuration: 5 })
+    mcounter()
+    expect(c).toEqual(1)
+    mcounter()
+    //Let's wait the promise resolution
+    setTimeout(() => {
+      expect(c).toEqual(2)
+    }, 10)
   })
 })
