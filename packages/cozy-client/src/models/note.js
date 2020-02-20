40eb5e0ac570dd70e23ee52045085d89708fc529
Quentin Valmori <quentin.valmori@gmail.com>
2020-02-20 09:16:12 +0100

    feat: Add a few methods to Note Model

--- packages/cozy-client/src/models/note.js
+++ packages/cozy-client/src/models/note.js
@@ -1,3 +1,6 @@
+import { Schema } from 'prosemirror-model'
+import { generateWebLink } from '../helpers'
+import { marks, nodes } from './note_schema'
 /**
  *
  * @param {string} notesAppUrl URL to the Notes App (https://notes.foo.mycozy.cloud)
@@ -6,3 +9,82 @@
 export const generateUrlForNote = (notesAppUrl, file) => {
   return notesAppUrl + '#/n/' + file._id
 }
+
+/**
+ * Fetch and build an URL to open a note.
+ *
+ * @param {object} client CozyClient instance
+ * @param {object} file io.cozy.file object
+ * @returns {string} url
+ */
+export const fetchUrlToOpenANote = async (client, file) => {
+  const {
+    data: { note_id, subdomain, protocol, instance, sharecode, public_name }
+  } = await client
+    .getStackClient()
+    .collection('io.cozy.notes')
+    .fetchURL({ _id: file.id })
+  const searchParams = [['id', note_id]]
+  if (sharecode) searchParams.push(['sharecode', sharecode])
+  if (public_name) searchParams.push(['username', public_name])
+  const pathname = sharecode ? '/public/' : ''
+  const url = generateWebLink({
+    cozyUrl: `${protocol}://${instance}`,
+    searchParams,
+    pathname,
+    hash: `/n/${note_id}`,
+    slug: 'notes',
+    subDomainType: subdomain
+  })
+  return url
+}
+
+/**
+ * @typedef {object} CozyClient
+ *
+ * Create a note and return the created object
+ * @param {CozyClient} client
+ * @param {string} dir_id
+ */
+export function createNoteDocument(client, dir_id) {
+  return client.getStackClient().fetchJSON('POST', '/notes', {
+    data: {
+      type: 'io.cozy.notes.documents',
+      attributes: {
+        title: '',
+        schema: schemaOrdered,
+        dir_id
+      }
+    }
+  })
+}
+
+export const generateUrlForNoteWithReturnUrl = (
+  noteAppUrl,
+  file,
+  returnUrl
+) => {
+  const url = new URL(generateUrlForNote(noteAppUrl, file))
+  url.searchParams.set('returnUrl', returnUrl)
+  url.hash = `#/n/${file.id}`
+  return url
+}
+
+function orderedToObject(ordered) {
+  return ordered.reduce(function(acc, cur) {
+    acc[cur[0]] = cur[1]
+    return acc
+  }, {})
+}
+
+export const schemaOrdered = {
+  nodes,
+  marks
+}
+
+export const schemaObject = {
+  nodes: orderedToObject(nodes),
+  marks: orderedToObject(marks)
+}
+
+export const schema = new Schema(schemaObject)
--- packages/cozy-client/src/models/note.spec.js
+++ packages/cozy-client/src/models/note.spec.js
@@ -1,4 +1,5 @@
 import { note } from './'
+
 describe('note model', () => {
   it('should generate the right link to a note', () => {
     const noteDocument = {
@@ -10,5 +11,78 @@ describe('note model', () => {
     expect(note.generateUrlForNote(notesAppUrl, noteDocument)).toEqual(
       `http://notes.cozy.tools/#/n/${noteDocument._id}`
     )
+  })
+
+  describe('fetchUrlToOpenANote', () => {
+    it('should build the right url from the stack response', async () => {
+      const fetchURLspy = jest.fn()
+      const mockedClient = {
+        getStackClient: () => ({
+          collection: name => ({
+            fetchURL: fetchURLspy
+          })
+        })
+      }
+      fetchURLspy.mockResolvedValue({
+        data: { note_id: 1, protocol: 'https', instance: 'foo.mycozy' }
+      })
+      const generatedUrl = await note.fetchUrlToOpenANote(mockedClient, {
+        id: 1
+      })
+
+      expect(generatedUrl.toString()).toEqual(
+        'https://foo-notes.mycozy/?id=1#/n/1'
+      )
+
+      fetchURLspy.mockResolvedValue({
+        data: {
+          note_id: 1,
+          protocol: 'https',
+          instance: 'foo.mycozy',
+          sharecode: 'hahaha'
+        }
+      })
+      const generatedUrl2 = await note.fetchUrlToOpenANote(mockedClient, {
+        id: 1
+      })
+
+      expect(generatedUrl2.toString()).toEqual(
+        'https://foo-notes.mycozy/public/?id=1&sharecode=hahaha#/n/1'
+      )
+
+      fetchURLspy.mockResolvedValue({
+        data: {
+          note_id: 1,
+          protocol: 'https',
+          instance: 'foo.mycozy',
+          sharecode: 'hahaha',
+          public_name: 'Crash'
+        }
+      })
+      const generatedUrl3 = await note.fetchUrlToOpenANote(mockedClient, {
+        id: 1
+      })
+
+      expect(generatedUrl3.toString()).toEqual(
+        'https://foo-notes.mycozy/public/?id=1&sharecode=hahaha&username=Crash#/n/1'
+      )
+    })
+  })
+
+  describe('generateUrlForNoteWithReturnUrl', () => {
+    it('should generate an URL with the returnkey', () => {
+      const generatedUrl = note.generateUrlForNoteWithReturnUrl(
+        'https://notes.cozy.foo',
+        {
+          id: 1,
+          _id: 1
+        },
+        'https://drive.cozy.foo/#/folder/1'
+      )
+
+      expect(generatedUrl.toString()).toEqual(
+        'https://notes.cozy.foo/?returnUrl=https%3A%2F%2Fdrive.cozy.foo%2F%23%2Ffolder%2F1#/n/1'
+      )
+    })
   })
 })
