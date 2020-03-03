b022232d9261475aedfef68bb4fc24daedfded6c
Patrick Browne <pt.browne@gmail.com>
2020-03-03 11:24:13 +0100

    feat: Better UI/token prompt

--- packages/cozy-client/examples/web/src/DebugClientProvider.jsx
+++ packages/cozy-client/examples/web/src/DebugClientProvider.jsx
@@ -15,12 +15,21 @@ class ExpiredTokenHandler extends Component {
   }
   render() {
     return this.props.children
+const styles = {
+  uriTokenPrompt: {
+    maxWidth: '800px',
+    margin: 'auto'
   }
 }
 
+/**
+ * Guards its children with a form asking for token/uri
+ * When user has entered token/uri, provides its children with a CozyClient
+ * through CozyProvider
+ */
 const DebugClientProvider = ({ children }) => {
   const [token, setToken] = useStickyState(null, 'clientToken')
-  const [uri, setURI] = useStickyState(null, 'clientURI')
+  const [uri, setURI] = useStickyState('http://cozy.tools:8080', 'clientURI')
   const [client, setClient] = useState(null)
 
   useEffect(() => {
@@ -56,11 +65,28 @@ const DebugClientProvider = ({ children }) => {
       </ExpiredTokenHandler>
     )
   } else {
-    return <>
-      token: <input type='text' onChange={e => { setToken(e.target.value)}}/><br/>
-      uri: <input type='text' onChange={e => { setURI(e.target.value)}}/>
-    </>
+    return <URITokenPrompt
+      initialToken={token}
+      initialURI={uri}
+      onChangeToken={setToken}
+      onChangeURI={setURI} />
   }
+  }
+}
+
+
+const URITokenPrompt = ({ onChangeToken, onChangeURI, initialURI, initialToken  }) => {
+  return <>
+      <div style={styles.uriTokenPrompt}>
+      <p>
+        Please enter an URI and a token (<span style={{ fontFamily: 'monospace' }}>cozy-stack instances token-cli</span>)<br/>
+        <label>
+          uri: <input defaultValue={initialURI} type='text' onChange={e => { onChangeURI(e.target.value)}}/>
+        </label><br/>
+        <label>token: <input defaultValue={initialToken} type='text' onChange={e => { onChangeToken(e.target.value)}}/></label>
+      </p>
+      </div>
+    </>
 }
 
 export default DebugClientProvider
