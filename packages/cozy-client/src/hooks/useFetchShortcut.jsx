8fda21d7f44368a0e2a00662c54614cc3c13144e
Yannick Lohse <contact@yannick-lohse.fr>
2020-03-06 11:39:49 +0100

    fix: Removed timeout

--- packages/cozy-client/src/hooks/useFetchShortcut.jsx
+++ packages/cozy-client/src/hooks/useFetchShortcut.jsx
@@ -4,9 +4,7 @@ const useFetchShortcut = (client, id) => {
   const [shortcutInfos, setShortcutInfos] = useState()
   const [shortcurtImg, setShotcutImg] = useState()
   const [fetchStatus, setFetchStatus] = useState('idle')
-  const [shouldDisplayImg, setShouldDisplayImg] = useState(false)
   useEffect(() => {
-    let timeout
     const fetchData = async () => {
       setFetchStatus('loading')
       try {
@@ -22,10 +20,6 @@ const useFetchShortcut = (client, id) => {
         }/icon.png`
 
         setShotcutImg(imgUrl)
-        // this is used in conjonction with CSS display none/block to
-        // make the http request to load the image and not having a blank
-        // element for a few ms
-        timeout = setTimeout(() => setShouldDisplayImg(true), 400)
         setShortcutInfos(shortcutInfosResult)
         setFetchStatus('loaded')
       } catch (e) {
@@ -33,14 +27,12 @@ const useFetchShortcut = (client, id) => {
       }
     }
     fetchData()
-    return () => clearTimeout(timeout)
   }, [client, id])
 
   return {
     shortcutInfos,
     shortcurtImg,
-    fetchStatus,
-    shouldDisplayImg
+    fetchStatus
   }
 }
 
