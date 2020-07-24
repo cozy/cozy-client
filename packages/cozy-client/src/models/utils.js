7c17fc86a7fbfca2e09f000ece87e91c8f8ad951
Patrick Browne <pt.browne@gmail.com>
2020-07-24 12:30:05 +0200

    feat: Add hasBeenUpdatedByApp helper

--- packages/cozy-client/src/models/utils.js
+++ packages/cozy-client/src/models/utils.js
@@ -1,3 +1,8 @@
 import get from 'lodash/get'
 
+export const hasBeenUpdatedByApp = (doc, appSlug) => {
+  const updatedByApps = get(doc, 'cozyMetadata.updatedByApps')
+  return Boolean(updatedByApps && updatedByApps.find(x => x.slug === appSlug))
+}
+
 export const getCreatedByApp = doc => get(doc, 'cozyMetadata.createdByApp')
