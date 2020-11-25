714fdf86970a1c307d71d6adcfcfe2011f037743
Gara64 <paul@cozycloud.cc>
2020-11-25 11:50:16 +0100

    feat: Add partial index example

--- packages/cozy-client/examples/perfs.js
+++ packages/cozy-client/examples/perfs.js
@@ -1,90 +1,15 @@
-const { QueryDefinition, default: CozyClient } = require('../dist')
+const { createClientInteractive } = require('cozy-client')
+const {
+  schema,
+  createContactsBulk,
+  deleteAllContactsBulk,
+  getAllContacts,
+  getAllContactsSkip,
+  getAllContactsBookmark,
+  getFilteredContactsBookmark
+} = require('./contacts')
+
 global.fetch = require('node-fetch') // in the browser we have native fetch
-
-let client = {}
-
-const schema = {
-  contacts: {
-    doctype: 'io.cozy.contacts'
-  }
-}
-
-const createContactsBulk = async count => {
-  const contacts = []
-  for (let i = 0; i < count; i++) {
-    const familyName = `Fake${i}`
-    const givenName = `Faky${i}`
-    const address = `${familyName}@${givenName}.com`
-    const contact = {
-      name: { familyName, givenName },
-      email: [{ address }]
-    }
-    contacts.push(contact)
-  }
-  await client.stackClient.collection('io.cozy.contacts').updateAll(contacts)
-}
-
-const deleteAllContactsBulk = async () => {
-  const contacts = await getAllContacts()
-  await client.stackClient.collection('io.cozy.contacts').destroyAll(contacts)
-}
-
-const getAllContacts = async () => {
-  const query = new QueryDefinition({
-    doctype: 'io.cozy.contacts',
-    limit: null
-  })
-  const resp = await client.query(query)
-  return resp.data
-}
-
-const getAllContactsSkip = async () => {
-  const query = new QueryDefinition({
-    doctype: 'io.cozy.contacts',
-    limit: 100
-  })
-  const contacts = []
-  let resp = { next: true }
-
-  while (resp && resp.next) {
-    resp = await client.query(query.offset(contacts.length), {
-      as: 'skip-query'
-    })
-    contacts.push(...resp.data)
-  }
-  return contacts
-}
-
-const getAllContactsBookmark = async () => {
-  const query = new QueryDefinition({
-    doctype: 'io.cozy.contacts',
-    limit: 100
-  })
-  const contacts = await client.queryAll(query, { as: 'bookmark-all-query' })
-  return contacts
-}
-
-const getFilteredContactsBookmark = async () => {
-  const selector = {
-    _id: {
-      $gt: null
-    }
-  }
-  const query = new QueryDefinition({
-    doctype: 'io.cozy.contacts',
-    limit: 100,
-    selector
-  })
-  const contacts = []
-  let resp = { next: true, bookmark: '' }
-  while (resp && resp.next) {
-    resp = await client.query(query.offsetBookmark(resp.bookmark), {
-      as: 'bookmark-find-query'
-    })
-    contacts.push(...resp.data)
-  }
-  return contacts
-}
 
 /**
  * Measure query performances
@@ -115,29 +40,29 @@ const main = async _args => {
 
   const count = 1000
 
-  await deleteAllContactsBulk()
-  await createContactsBulk(count)
+  await deleteAllContactsBulk(client)
+  await createContactsBulk(client, count)
   // reset store to avoid side effects from previous queries
   client.store = null
 
   console.time(count + ' contacts no pagination')
-  await getAllContacts()
+  await getAllContacts(client)
   console.timeEnd(count + ' contacts no pagination')
 
   client.store = null
 
   console.time(count + ' contacts skip')
-  await getAllContactsSkip()
+  await getAllContactsSkip(client)
   console.timeEnd(count + ' contacts skip')
 
   client.store = null
 
   console.time(count + ' all contacts bookmark')
-  await getAllContactsBookmark()
+  await getAllContactsBookmark(client)
   console.timeEnd(count + ' all contacts bookmark')
 
   console.time(count + ' filtered contacts bookmark')
-  await getFilteredContactsBookmark()
+  await getFilteredContactsBookmark(client)
   console.timeEnd(count + ' filtered contacts bookmark')
 }
 
