3b1a030811e8f7a39f3399bd100c2d02f3665395
paultranvan <paul@cozycloud.cc>
2021-02-22 15:38:02 +0100

    feat: Add TimeSerie helpers
    We introduce a new doctype in Cozy to manipulate time series.

---
+++ packages/cozy-client/src/models/timeseries.js
@@ -0,0 +1,74 @@
+import { Q } from '../queries/dsl'
+
+const validateTimeSerieFormat = timeserie => {
+  if (!timeserie.startDate || !timeserie.endDate) {
+    throw new Error(
+      'You must specify a startDate and endDate for the time serie'
+    )
+  }
+  if (!Date.parse(timeserie.startDate) || !Date.parse(timeserie.endDate)) {
+    throw new Error('Invalid date format for the time serie')
+  }
+  if (!timeserie.dataType) {
+    throw new Error('You must specify a dataType for the time serie')
+  }
+  if (!timeserie.serie || !Array.isArray(timeserie.serie)) {
+    throw new Error('You must specify a serie array for the time serie')
+  }
+}
+
+/**
+ * Helper to save a time serie document.
+ *
+ * @param {object} client - The CozyClient instance
+ * @param  {{dataType, serie, startDate, endDate, source, theme}} - The time serie attributes
+ */
+export const saveTimeSerie = async (
+  client,
+  { dataType, serie, startDate, endDate, source, theme }
+) => {
+  validateTimeSerieFormat({ dataType, serie, startDate, endDate, source })
+
+  const doctype = `io.cozy.timeseries.${dataType}`
+  const timeserie = {
+    _type: doctype,
+    startDate,
+    endDate,
+    source,
+    theme,
+    serie
+  }
+  return client.save(timeserie)
+}
+
+/**
+ * Helper to retrieve time series by their date interval and source.
+ *
+ * The starting date must be greater or equal while the ending date must
+ * be stricly less than the given startDate and endDate parameters.
+ *
+ * @param {object} client - The CozyClient instance
+ * @param {{ startDate, endDate, dataType, source, limit }} The query params.
+ * @returns {object} The time series found by the query
+ */
+export const fetchTimeSerieByIntervalAndSource = async (
+  client,
+  { startDate, endDate, dataType, source, limit }
+) => {
+  const doctype = `io.cozy.timeseries.${dataType}`
+  const query = Q(doctype)
+    .where({
+      source: source,
+      startDate: {
+        $gte: startDate
+      },
+      endDate: {
+        $lt: endDate
+      }
+    })
+    .indexFields(['source', 'startDate', 'endDate'])
+    .sortBy([{ source: 'desc' }, { startDate: 'desc' }, { endDate: 'desc' }])
+    .limitBy(limit || 5)
+
+  return client.query(query)
+}
