5a796c27bbc88587cbe34175955a04447a4e24c2
Patrick Browne <pt.browne@gmail.com>
2021-03-01 18:55:24 +0100

    chore: Lint

--- scripts/docs.js
+++ scripts/docs.js
@@ -4,30 +4,37 @@ const path = require('path')
 const glob = require('glob')
 const fs = require('fs-extra')
 
-const globPromise = (pattern, options) => new Promise((resolve, reject) => {
-  glob.glob(pattern, {}, (err, files) => {
-    if (err) { reject(err) }
-    else {
-      resolve(files)
-    }
+const globPromise = (pattern, options) =>
+  new Promise((resolve, reject) => {
+    glob.glob(pattern, {}, (err, files) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(files)
+      }
+    })
   })
-})
 
 const docsFolder = path.join(__dirname, '../docs')
 const main = async () => {
   const packages = await globPromise('packages/*')
   await fs.mkdirp(path.resolve(docsFolder, 'api'))
   for (let pkg of packages) {
-    const files = await globPromise(`${pkg}/src/**/*.js*`, { ignore: ['*.spec.js']})
-    if (files.length === 0) { continue }
+    const files = await globPromise(`${pkg}/src/**/*.js*`, {
+      ignore: ['*.spec.js']
+    })
+    if (files.length === 0) {
+      continue
+    }
     const pkgName = pkg.split('/')[1]
-    const pkgDoc = path.join(docsFolder, pkgName)
     const template = `{{>main}}`
     const templateData = jsdoc2md.getTemplateDataSync({ files })
-    const output = jsdoc2md.renderSync({ data: templateData, template: template })
+    const output = jsdoc2md.renderSync({
+      data: templateData,
+      template: template
+    })
     fs.writeFileSync(path.resolve(docsFolder, 'api', `${pkgName}.md`), output)
   }
-
 }
 
 main().catch(error => {
