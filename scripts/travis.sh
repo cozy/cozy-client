65feebf1cf47d21f77a1a080990f0b001e988384
Patrick Browne <pt.browne@gmail.com>
2021-03-01 18:10:34 +0100

    chore: Fix travis script for exit 1 on outdated docs
    Since the false command was not the last command, its exit status
    was not "returned" as the exit status of the whole process

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+
 yarn lint
 yarn build # before tests since packages rely on dist/ files to be available
 yarn test --runInBand
@@ -8,7 +9,15 @@ yarn docs
 
 
 set +e # The following command relies on exit 1
-(git diff --exit-code -- docs && echo "Docs are up-to-date") || (echo "Docs are not up-to-date, please run yarn docs and repush" && false)
+git diff --exit-code -- docs
+docs_status=$?
+set -e
+if [[ $docs_status == 0 ]]; then
+  echo "Docs are up-to-date"
+else
+  echo "Docs are not up-to-date, please run yarn docs and repush"
+  exit 1
+fi
 set -e
 
 cd packages/cozy-client
