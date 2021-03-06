534d9342cfea08e740b0c5760ba262faa0f207c7
Patrick Browne <pt.browne@gmail.com>
2021-03-06 16:10:03 +0100

    feat: Type useClient return value

--- packages/cozy-client/src/hooks/useClient.js
+++ packages/cozy-client/src/hooks/useClient.js
@@ -1,6 +1,11 @@
 import { useContext } from 'react'
 import clientContext from '../context'
-
+import CozyClient from '../CozyClient'
+/**
+ * Returns the cozy client from the context
+ *
+ * @returns {CozyClient} [description]
+ */
 const useClient = () => {
   const { client } = useContext(clientContext)
   return client
