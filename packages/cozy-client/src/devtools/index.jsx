c2e8a74edc4216b49f983c4368f4f22b71ed45eb
Patrick Browne <pt.browne@gmail.com>
2021-03-26 19:04:18 +0100

    feat: Add devtools
    - Add a devtool panel that can be opened by clicking on a Fab button
    that is only displayed when the debug flag is set to true.
    - The devtools is made of several "panels". There are deffault panels
    and the app can also inject its own adhoc panels.
    - The "queries" panel shows the current queries inside ccozy-client
    cache.
    It is inspired by react-query devtools and should in the
    future be improved to allow for action on queries: reset, set to empty,
    delete etc... It could also show performance tips for each query (based
    on the definition for example).
    - The "flags" panel shows all the current flags and allow to modify them.
    - The "libraries" panel show library versions based on the
    global __VERSIONS__ variable that should be injected by the app. If it
    is defined, the panel will be blank.

---
+++ packages/cozy-client/src/devtools/index.jsx
@@ -0,0 +1,113 @@
+import React, { useState, useCallback, useMemo } from 'react'
+
+import Fab from '@material-ui/core/Fab'
+import Paper from '@material-ui/core/Paper'
+import IconButton from '@material-ui/core/IconButton'
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
+
+import CozyTheme from 'cozy-ui/transpiled/react/CozyTheme'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import List from 'cozy-ui/transpiled/react/MuiCozyTheme/List'
+import Typography from 'cozy-ui/transpiled/react/Typography'
+import ListItem from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItem'
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+import GearIcon from 'cozy-ui/transpiled/react/Icons/Gear'
+import CrossIcon from 'cozy-ui/transpiled/react/Icons/Cross'
+
+import { NavSecondaryAction, ListGridItem } from './common'
+import useLocalState from './useLocalState'
+
+/**
+ * @type {Object.<string, React.CSSProperties>}
+ * @private
+ */
+const styles = {
+  fab: { position: 'fixed', left: '1rem', bottom: '1rem' },
+  panel: { position: 'fixed', bottom: 0, height: 300, left: 0, right: 0 },
+  closeIcon: { position: 'absolute', top: '0.5rem', right: '0.5rem' },
+  panelContainer: { height: '100%', flexWrap: 'nowrap' },
+  panelRight: { height: '100%', overflow: 'scroll', flexGrow: 1 },
+  mono: { fontFamily: 'monospace' }
+}
+
+const defaultPanels = [
+]
+
+const DevToolsNavList = ({ selected, panels, onNav }) => {
+  return (
+    <List>
+      {panels.map(panel => {
+        return (
+          <ListItem
+            key={panel.name}
+            selected={selected === panel.id}
+            dense
+            button
+            onClick={() => onNav(panel.id)}
+          >
+            <ListItemText>{panel.id}</ListItemText>
+            <NavSecondaryAction />
+          </ListItem>
+        )
+      })}
+    </List>
+  )
+}
+
+const DevToolsPanel = props => {
+  const panels = useMemo(() => {
+    if (props.panels) {
+      return [...defaultPanels, ...props.panels]
+    }
+    return defaultPanels
+  }, [props.panels])
+  const [currentPanel, setCurrentPanel] = useState('queries')
+  return (
+    <Paper elevation={12} {...props}>
+      <IconButton style={styles.closeIcon} onClick={props.onClose}>
+        <Icon icon={CrossIcon} size={12} />
+      </IconButton>
+      <Grid container style={styles.panelContainer}>
+        <ListGridItem>
+          <Box p={1}>
+            <Typography variant="subtitle1">Cozy Devtools</Typography>
+          </Box>
+          <DevToolsNavList
+            panels={panels}
+            selected={currentPanel}
+            onNav={setCurrentPanel}
+          />
+        </ListGridItem>
+        {panels.map(panelOptions =>
+          currentPanel === panelOptions.id ? <panelOptions.Component /> : null
+        )}
+      </Grid>
+    </Paper>
+  )
+}
+
+const DevTools = ({ panels }) => {
+  const [open, setOpen] = useLocalState('cozydevtools__open', false)
+  const handleToggle = useCallback(() => setOpen(state => !state), [setOpen])
+  return (
+    <>
+      <Fab color="primary" onClick={handleToggle} style={styles.fab}>
+        <Icon icon={GearIcon} />
+      </Fab>
+      {open ? (
+        <CozyTheme variant="normal">
+          <DevToolsPanel
+            style={styles.panel}
+            onClose={handleToggle}
+            panels={panels}
+          />
+        </CozyTheme>
+      ) : null}
+    </>
+  )
+}
+
+export default DevTools
+export { NavSecondaryAction, ListGridItem, useLocalState }
+export { default as PanelContent } from './PanelContent'
