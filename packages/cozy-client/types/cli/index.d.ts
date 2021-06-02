0c86766f19a4a8ff1645299a9670fca75133e1bb
Patrick Browne <pt.browne@gmail.com>
2021-06-02 10:51:49 +0200

    chore: Update typescript defs

--- packages/cozy-client/types/cli/index.d.ts
+++ packages/cozy-client/types/cli/index.d.ts
@@ -23,5 +23,5 @@ export type DestroyableServer = any;
  * })
  * ```
  */
-export function createClientInteractive(clientOptions: object, serverOpts: any): CozyClient | Promise<any>;
+export function createClientInteractive(clientOptions: object, serverOpts: any): Promise<any> | CozyClient;
 import CozyClient from "../CozyClient";
