10a141626b697f242c8a0869cfc46087b06023fd
doubleface <christophe@cozycloud.cc>
2021-06-08 12:55:32 +0200

    fix: Types

--- packages/cozy-client/types/authentication/mobile.d.ts
+++ packages/cozy-client/types/authentication/mobile.d.ts
@@ -1,3 +1,2 @@
-export function authenticateWithReactNativeInAppBrowser(url: string): Promise<any>;
 export function authenticateWithCordova(url: any): Promise<any>;
-export function authFunction(url: string): Promise<any>;
+export function authFunction(url: any): Promise<any>;
