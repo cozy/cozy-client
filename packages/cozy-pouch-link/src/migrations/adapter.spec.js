ee14bbed6736fdee8b6518c97b81f6bed18510c8
paultranvan <paul@cozycloud.cc>
2021-07-21 17:05:02 +0200

    test: Add migration test
    fix: Useless import

---
+++ packages/cozy-pouch-link/src/migrations/adapter.spec.js
@@ -0,0 +1,19 @@
+import { migratePouch } from './adapter'
+import PouchDB from 'pouchdb-browser'
+
+describe('adapter', () => {
+  PouchDB.plugin(require('pouchdb-adapter-memory'))
+
+  it('should migrate', async () => {
+    const oldPouch = new PouchDB('test', { adapter: 'memory' })
+    await oldPouch.put({
+      _id: 'hellodoc',
+      name: 'bugs'
+    })
+    const migrated = await migratePouch(oldPouch, 'memory')
+    expect(migrated.adapter).toEqual('memory')
+    const doc = await migrated.get('hellodoc')
+    expect(doc._id).toEqual('hellodoc')
+    expect(doc.name).toEqual('bugs')
+  })
+})
