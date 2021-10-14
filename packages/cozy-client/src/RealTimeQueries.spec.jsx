cozy/cozy-client

commit e23239fc2a17f8362a74dabdda0e3f816dfc6eb2

fix: RealTimeQueries accepts more than files
  The `RealTimeQueries` component would receive realtime changes through
  its `realtime` plugin subscription but would normalize every CouchDB
  document received as an `io.cozy.files` document rendering it unusable
  for any other doctype.

  We now make sure the doctype passed in the subscription is used as the
  received documents' doctype.

packages/cozy-client/src/RealTimeQueries.jsx
@@ -1,21 +1,42 @@
 import { memo, useEffect } from 'react'
 import useClient from './hooks/useClient'
-import { file as fileModel } from './models'
 import { Mutations } from './queries/dsl'
 import { receiveMutationResult } from './store'
 import CozyClient from './CozyClient'
-import { CozyClientDocument, Mutation } from './types'
+import { CouchDBDocument, CozyClientDocument, Doctype, Mutation } from './types'
+
+/**
+ * Normalizes an object representing a CouchDB document
+ *
+ * Ensures existence of `_type`
+ *
+ * @public
+ * @param {CouchDBDocument} couchDBDoc - object representing the document
+ * @returns {CozyClientDocument} full normalized document
+ */
+const normalizeDoc = (couchDBDoc, doctype) => {
+  return {
+    _type: doctype,
+    ...couchDBDoc
+  }
+}
 
 /**
  * DispatchChange
  *
  * @param {CozyClient} client CozyClient instane
- * @param {CozyClientDocument} document Document to update
+ * @param {CouchDBDocument} couchDBDoc Document to update
  * @param {Mutation} mutationDefinitionCreator Mutation to apply
  */
-const dispatchChange = (client, document, mutationDefinitionCreator) => {
+const dispatchChange = (
+  client,
+  doctype,
+  couchDBDoc,
+  mutationDefinitionCreator
+) => {
+  const data = normalizeDoc(couchDBDoc, doctype)
   const response = {
-    data: fileModel.normalize(document)
+    data
   }
 
   const options = {}
@@ -24,7 +45,7 @@ const dispatchChange = (client, document, mutationDefinitionCreator) => {
       client.generateRandomId(),
       response,
       options,
-      mutationDefinitionCreator(document)
+      mutationDefinitionCreator(data)
     )
   )
 }
@@ -34,7 +55,7 @@ const dispatchChange = (client, document, mutationDefinitionCreator) => {
  * internal store updated.
  *
  * @param  {object} options - Options
- * @param  {string} options.doctype - The doctype to watch
+ * @param  {Doctype} options.doctype - The doctype to watch
  * @returns {null} The component does not display anything.
  */
 const RealTimeQueries = ({ doctype }) => {
@@ -49,16 +70,17 @@ const RealTimeQueries = ({ doctype }) => {
       )
     }
 
-    const dispatchCreate = document => {
-      dispatchChange(client, document, Mutations.createDocument)
+    const dispatchCreate = couchDBDoc => {
+      dispatchChange(client, doctype, couchDBDoc, Mutations.createDocument)
     }
-    const dispatchUpdate = document => {
-      dispatchChange(client, document, Mutations.updateDocument)
+    const dispatchUpdate = couchDBDoc => {
+      dispatchChange(client, doctype, couchDBDoc, Mutations.updateDocument)
     }
-    const dispatchDelete = document => {
+    const dispatchDelete = couchDBDoc => {
       dispatchChange(
         client,
-        { ...document, _deleted: true },
+        doctype,
+        { ...couchDBDoc, _deleted: true },
         Mutations.deleteDocument
       )
     }

packages/cozy-client/src/types.js
@@ -7,7 +7,9 @@ import { QueryDefinition } from './queries/dsl'
  * @typedef {"io.cozy.notes"} NotesDoctype
  * @typedef {"io.cozy.apps"} AppsDoctype
  * @typedef {"io.cozy.settings"} SettingsDoctype
- * @typedef {AccountsDoctype|TriggersDoctype|KonnectorsDoctype|NotesDoctype|AppsDoctype|SettingsDoctype} KnownDoctype
+ * @typedef {"io.cozy-oauth.clients"} OAuthClientsDoctype
+ * @typedef {"io.cozy.files"} FilesDoctype
+ * @typedef {AccountsDoctype|TriggersDoctype|KonnectorsDoctype|NotesDoctype|AppsDoctype|SettingsDoctype|OAuthClientsDoctype|FilesDoctype} KnownDoctype
  * @typedef {KnownDoctype|string} Doctype
  */
 
@@ -113,13 +115,14 @@ import { QueryDefinition } from './queries/dsl'
  * @property {string} [_id] - Id of the document
  * @property {string} [_type] - Type of the document
  * @property {string} [_rev] - Current revision of the document
- * @property {string} [_deleted] - When the document has been deleted
+ * @property {boolean} [_deleted] - When the document has been deleted
  * @property {object} [relationships] - Relationships of the document
  */
 
 /**
  * @typedef {object} FileDocument - An io.cozy.files document
  * @property {string} _id - Id of the file
+ * @property {FilesDoctype} _type - Doctype of the file
  * @property {string} name - Name of the file
  * @property {object} metadata - Metadata of the file
  * @property {object} type - Type of the file
@@ -130,10 +133,29 @@ import { QueryDefinition } from './queries/dsl'
 /**
  * @typedef {object} FolderDocument - An io.cozy.files document
  * @property {string} _id - Id of the folder
+ * @property {FilesDoctype} _type - Doctype of the folder
  * @property {string} name - Name of the folder
  * @property {object} metadata - Metadata of the folder
  * @property {object} type - Type of the folder
  * @typedef {CozyClientDocument & FileDocument} IOCozyFolder - An io.cozy.files document
+ */
+
+/**
+ * @typedef {object} OAuthClientDocument - An io.cozy.oauth.clients document
+ * @property {string} _id - Id of the client
+ * @property {OAuthClientsDoctype} _type - Doctype of the client
+ * @property {string} software_id
+ * @property {string} software_version
+ * @property {string} client_id
+ * @property {string} client_name
+ * @property {string} client_kind
+ * @property {string} client_uri
+ * @property {string} logo_uri
+ * @property {string} policy_uri
+ * @property {string} notification_platform
+ * @property {string} notification_device_token
+ * @property {Array<String>} redirect_uris
+ * @typedef {CozyClientDocument & OAuthClientDocument} IOCozyOAuthClient - An io.cozy.oauth.clients document
  */
 
 /**
@@ -182,6 +204,14 @@ import { QueryDefinition } from './queries/dsl'
  * @property {object} SafariViewController
  * @property {Function} resolveLocalFileSystemURL
  * @property {Function} handleOpenURL
+ */
+
+/**
+ * @typedef {object} CouchDBDocument - A document
+ * @property {string} _id - Id of the document
+ * @property {string} _rev - Current revision of the document
+ * @property {boolean} [_deleted] - When the document has been deleted
+ * @property {object} [relationships] - Relationships of the document
  */
 
 /**

packages/cozy-client/types/types.d.ts
@@ -6,7 +6,9 @@ export type KonnectorsDoctype = "io.cozy.konnectors";
 export type NotesDoctype = "io.cozy.notes";
 export type AppsDoctype = "io.cozy.apps";
 export type SettingsDoctype = "io.cozy.settings";
-export type KnownDoctype = "io.cozy.accounts" | "io.cozy.triggers" | "io.cozy.konnectors" | "io.cozy.notes" | "io.cozy.apps" | "io.cozy.settings";
+export type OAuthClientsDoctype = "io.cozy-oauth.clients";
+export type FilesDoctype = "io.cozy.files";
+export type KnownDoctype = "io.cozy.files" | "io.cozy.accounts" | "io.cozy.triggers" | "io.cozy.konnectors" | "io.cozy.notes" | "io.cozy.apps" | "io.cozy.settings" | "io.cozy-oauth.clients";
 export type Doctype = string;
 export type Link = any;
 export type Mutation = any;
@@ -108,7 +110,7 @@ export type CozyClientDocument = {
     /**
      * - When the document has been deleted
      */
-    _deleted?: string;
+    _deleted?: boolean;
     /**
      * - Relationships of the document
      */
@@ -122,6 +124,10 @@ export type FileDocument = {
      * - Id of the file
      */
     _id: string;
+    /**
+     * - Doctype of the file
+     */
+    _type: FilesDoctype;
     /**
      * - Name of the file
      */
@@ -152,6 +158,10 @@ export type FolderDocument = {
      */
     _id: string;
     /**
+     * - Doctype of the folder
+     */
+    _type: FilesDoctype;
+    /**
      * - Name of the folder
      */
     name: string;
@@ -168,6 +178,34 @@ export type FolderDocument = {
  * - An io.cozy.files document
  */
 export type IOCozyFolder = CozyClientDocument & FileDocument;
+/**
+ * - An io.cozy.oauth.clients document
+ */
+export type OAuthClientDocument = {
+    /**
+     * - Id of the client
+     */
+    _id: string;
+    /**
+     * - Doctype of the client
+     */
+    _type: OAuthClientsDoctype;
+    software_id: string;
+    software_version: string;
+    client_id: string;
+    client_name: string;
+    client_kind: string;
+    client_uri: string;
+    logo_uri: string;
+    policy_uri: string;
+    notification_platform: string;
+    notification_device_token: string;
+    redirect_uris: Array<string>;
+};
+/**
+ * - An io.cozy.oauth.clients document
+ */
+export type IOCozyOAuthClient = CozyClientDocument & OAuthClientDocument;
 export type ClientError = {
     status?: string;
 };
@@ -192,6 +230,27 @@ export type CordovaWindow = {
     SafariViewController: object;
     resolveLocalFileSystemURL: Function;
     handleOpenURL: Function;
+};
+/**
+ * - A document
+ */
+export type CouchDBDocument = {
+    /**
+     * - Id of the document
+     */
+    _id: string;
+    /**
+     * - Current revision of the document
+     */
+    _rev: string;
+    /**
+     * - When the document has been deleted
+     */
+    _deleted?: boolean;
+    /**
+     * - Relationships of the document
+     */
+    relationships?: object;
 };
 /**
  * - An item of the CouchDB bulk docs response

packages/cozy-client/src/RealTimeQueries.spec.jsx
@@ -4,80 +4,96 @@ import RealTimeQueries from './RealTimeQueries'
 import { createMockClient } from './mock'
 import CozyProvider from './Provider'
 
+const setup = async doctype => {
+  const realtimeCallbacks = {}
+  const client = new createMockClient({})
+  client.plugins.realtime = {
+    subscribe: jest.fn((event, doctype, callback) => {
+      realtimeCallbacks[event] = callback
+    }),
+    unsubscribe: jest.fn()
+  }
+  client.dispatch = jest.fn()
+
+  const { unmount } = render(
+    <CozyProvider client={client}>
+      <RealTimeQueries doctype={doctype} />
+    </CozyProvider>
+  )
+
+  await waitFor(() =>
+    expect(client.plugins.realtime.subscribe).toHaveBeenCalledTimes(3)
+  )
+
+  return { client, realtimeCallbacks, unmount }
+}
+
 describe('RealTimeQueries', () => {
   it('notifies the cozy-client store', async () => {
-    const realtimeCallbacks = {}
-    const client = new createMockClient({})
-    client.plugins.realtime = {
-      subscribe: jest.fn((event, doctype, callback) => {
-        realtimeCallbacks[event] = callback
-      }),
-      unsubscribe: jest.fn()
-    }
-    client.dispatch = jest.fn()
+    const { client, realtimeCallbacks, unmount } = await setup('io.cozy.files')
 
-    const { unmount } = render(
-      <CozyProvider client={client}>
-        <RealTimeQueries doctype="io.cozy.files" />
-      </CozyProvider>
-    )
-
-    await waitFor(() =>
-      expect(client.plugins.realtime.subscribe).toHaveBeenCalledTimes(3)
-    )
-
-    realtimeCallbacks['created']({ id: 'mock-created', type: 'file' })
+    realtimeCallbacks['created']({ _id: 'mock-created', type: 'file' })
     expect(client.dispatch).toHaveBeenCalledWith(
       expect.objectContaining({
         definition: {
-          document: { id: 'mock-created', type: 'file' },
+          document: {
+            _id: 'mock-created',
+            _type: 'io.cozy.files',
+            type: 'file'
+          },
           mutationType: 'CREATE_DOCUMENT'
         },
         mutationId: '1',
         response: {
           data: {
+            _id: 'mock-created',
             _type: 'io.cozy.files',
-            type: 'file',
-            id: 'mock-created',
-            _id: 'mock-created'
+            type: 'file'
           }
         },
         type: 'RECEIVE_MUTATION_RESULT'
       })
     )
 
-    realtimeCallbacks['updated']({ id: 'mock-updated', type: 'file' })
+    realtimeCallbacks['updated']({ _id: 'mock-updated', type: 'file' })
     expect(client.dispatch).toHaveBeenCalledWith({
       definition: {
-        document: { id: 'mock-updated', type: 'file' },
+        document: {
+          _id: 'mock-updated',
+          _type: 'io.cozy.files',
+          type: 'file'
+        },
         mutationType: 'UPDATE_DOCUMENT'
       },
       mutationId: '2',
       response: {
         data: {
+          _id: 'mock-updated',
           _type: 'io.cozy.files',
-          type: 'file',
-          id: 'mock-updated',
-          _id: 'mock-updated'
+          type: 'file'
         }
       },
       type: 'RECEIVE_MUTATION_RESULT'
     })
 
-    realtimeCallbacks['deleted']({ id: 'mock-deleted', type: 'file' })
+    realtimeCallbacks['deleted']({ _id: 'mock-deleted', type: 'file' })
     expect(client.dispatch).toHaveBeenCalledWith({
       definition: {
-        document: { id: 'mock-deleted', type: 'file', _deleted: true },
+        document: {
+          _id: 'mock-deleted',
+          _type: 'io.cozy.files',
+          type: 'file',
+          _deleted: true
+        },
         mutationType: 'DELETE_DOCUMENT'
       },
       mutationId: '3',
       response: {
         data: {
-          _deleted: true,
+          _id: 'mock-deleted',
           _type: 'io.cozy.files',
           type: 'file',
-          id: 'mock-deleted',
-          _id: 'mock-deleted'
+          _deleted: true
         }
       },
       type: 'RECEIVE_MUTATION_RESULT'
@@ -85,5 +101,42 @@ describe('RealTimeQueries', () => {
 
     unmount()
     expect(client.plugins.realtime.unsubscribe).toHaveBeenCalledTimes(3)
+  })
+
+  it('deals with other doctypes than io.cozy.files', async () => {
+    const { client, realtimeCallbacks, unmount } = await setup(
+      'io.cozy.oauth.clients'
+    )
+
+    realtimeCallbacks['created']({
+      _id: 'mock-created',
+      client_kind: 'desktop',
+      client_name: 'Cozy Drive (hostname)'
+    })
+    expect(client.dispatch).toHaveBeenCalledWith(
+      expect.objectContaining({
+        definition: {
+          document: {
+            _id: 'mock-created',
+            _type: 'io.cozy.oauth.clients',
+            client_kind: 'desktop',
+            client_name: 'Cozy Drive (hostname)'
+          },
+          mutationType: 'CREATE_DOCUMENT'
+        },
+        mutationId: '1',
+        response: {
+          data: {
+            _id: 'mock-created',
+            _type: 'io.cozy.oauth.clients',
+            client_kind: 'desktop',
+            client_name: 'Cozy Drive (hostname)'
+          }
+        },
+        type: 'RECEIVE_MUTATION_RESULT'
+      })
+    )
+
+    unmount()
   })
 })

packages/cozy-client/types/RealTimeQueries.d.ts
@@ -1,4 +1,5 @@
 declare var _default: import("react").MemoExoticComponent<({ doctype }: {
-    doctype: string;
+    doctype: Doctype;
 }) => null>;
 export default _default;
+import { Doctype } from "./types";

packages/cozy-client/types/models/permission.d.ts
@@ -52,7 +52,7 @@ export type Document = {
     _type: string;
     type: string;
 };
-export type PermissionVerb = "DELETE" | "ALL" | "GET" | "PATCH" | "POST" | "PUT";
+export type PermissionVerb = "DELETE" | "PUT" | "POST" | "GET" | "ALL" | "PATCH";
 export type PermissionItem = {
     /**
      * - ALL, GET, PUT, PATCH, DELETE, POST…

commit f9d67f4c844c4c3b0c077d6c4af5d4c0ec521dbf

fix: Add id in CozyClientDocument for RealTime
Although we tend to rely on the `_id` for all documents, we sometimes
also rely on `id`, which is a legacy that we encounter in some apps such
as Drive. Therefore, e23239fc2a17f8362a74dabdda0e3f816dfc6eb2 introduced
a regression in Drive as the `id` was missing.

packages/cozy-client/src/RealTimeQueries.jsx
@@ -16,6 +16,7 @@ import { CouchDBDocument, CozyClientDocument, Doctype, Mutation } from './types'
  */
 const normalizeDoc = (couchDBDoc, doctype) => {
   return {
+    id: couchDBDoc._id,
     _type: doctype,
     ...couchDBDoc
   }

packages/cozy-client/src/types.js
@@ -113,6 +113,7 @@ import { QueryDefinition } from './queries/dsl'
 /**
  * @typedef {object} CozyClientDocument - A document
  * @property {string} [_id] - Id of the document
+ * @property {string} [id] - Id of the document
  * @property {string} [_type] - Type of the document
  * @property {string} [_rev] - Current revision of the document
  * @property {boolean} [_deleted] - When the document has been deleted

packages/cozy-client/types/types.d.ts
@@ -100,6 +100,10 @@ export type CozyClientDocument = {
      */
     _id?: string;
     /**
+     * - Id of the document
+     */
+    id?: string;
+    /**
      * - Type of the document
      */
     _type?: string;

packages/cozy-client/src/RealTimeQueries.spec.jsx
@@ -38,6 +38,7 @@ describe('RealTimeQueries', () => {
         definition: {
           document: {
             _id: 'mock-created',
+            id: 'mock-created',
             _type: 'io.cozy.files',
             type: 'file'
           },
@@ -47,6 +48,7 @@ describe('RealTimeQueries', () => {
         response: {
           data: {
             _id: 'mock-created',
+            id: 'mock-created',
             _type: 'io.cozy.files',
             type: 'file'
           }
@@ -60,6 +62,7 @@ describe('RealTimeQueries', () => {
       definition: {
         document: {
           _id: 'mock-updated',
+          id: 'mock-updated',
           _type: 'io.cozy.files',
           type: 'file'
         },
@@ -69,6 +72,7 @@ describe('RealTimeQueries', () => {
       response: {
         data: {
           _id: 'mock-updated',
+          id: 'mock-updated',
           _type: 'io.cozy.files',
           type: 'file'
         }
@@ -81,6 +85,7 @@ describe('RealTimeQueries', () => {
       definition: {
         document: {
           _id: 'mock-deleted',
+          id: 'mock-deleted',
           _type: 'io.cozy.files',
           type: 'file',
           _deleted: true
@@ -91,6 +96,7 @@ describe('RealTimeQueries', () => {
       response: {
         data: {
           _id: 'mock-deleted',
+          id: 'mock-deleted',
           _type: 'io.cozy.files',
           type: 'file',
           _deleted: true
@@ -118,6 +124,7 @@ describe('RealTimeQueries', () => {
         definition: {
           document: {
             _id: 'mock-created',
+            id: 'mock-created',
             _type: 'io.cozy.oauth.clients',
             client_kind: 'desktop',
             client_name: 'Cozy Drive (hostname)'
@@ -128,6 +135,7 @@ describe('RealTimeQueries', () => {
         response: {
           data: {
             _id: 'mock-created',
+            id: 'mock-created',
             _type: 'io.cozy.oauth.clients',
             client_kind: 'desktop',
             client_name: 'Cozy Drive (hostname)'