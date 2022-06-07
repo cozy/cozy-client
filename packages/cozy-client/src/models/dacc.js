f59d937cd0ef211ab5b79110e2aa33abfaec5459
Paul Tran-Van <paul@cozycloud.cc>
2022-06-07 17:03:02 +0200

    feat: Add method to fetch DACC aggregates
    This is a helper to query aggregate from the DACC thanks to a
    remote-doctype

--- packages/cozy-client/src/models/dacc.js
+++ packages/cozy-client/src/models/dacc.js
@@ -1,5 +1,14 @@
 import log from 'cozy-logger'
-import { DACCMeasure } from '../types'
+import { DACCMeasure, DACCAggregatesParams } from '../types'
+
+export const isCorrectDateFormat = date => {
+  try {
+    const parsedDate = new Date(Date.parse(date))
+    return parsedDate.toISOString().startsWith(date)
+  } catch (err) {
+    return false
+  }
+}
 
 /**
  * Throw an errror if a DACC parameter is incorrect.
@@ -26,8 +35,7 @@ export const checkMeasureParams = measure => {
   if (!startDate) {
     throw new Error('Missing parameter: startDate')
   }
-  const parsedDate = new Date(Date.parse(startDate))
-  if (!parsedDate.toISOString().startsWith(startDate)) {
+  if (!isCorrectDateFormat(startDate)) {
     throw new Error('Date should be in YYYY-MM-DD format')
   }
   if (typeof value !== 'number') {
@@ -68,6 +76,48 @@ export const sendMeasureToDACC = async (client, remoteDoctype, measure) => {
     log(
       'error',
       `Error while sending measure to remote doctype: ${error.message}`
+    )
+    throw error
+  }
+}
+
+export const buildAggregateParams = params => {
+  const { measureName } = params
+  if (!measureName || typeof measureName !== 'string') {
+    throw new Error('Missing or wrong type parameter: measureName')
+  }
+  const startDate = params.startDate || new Date(0).toISOString()
+  const endDate = params.endDate || new Date(Date.now()).toISOString()
+  if (!isCorrectDateFormat(startDate) || !isCorrectDateFormat(endDate)) {
+    throw new Error('Date should be in YYYY-MM-DD format')
+  }
+  return { measureName, startDate, endDate }
+}
+
+/**
+ * Send measures to a DACC through a remote doctype
+ *
+ * @param {object} client - The CozyClient instance
+ * @param {string} remoteDoctype - The remote doctype to use
+ * @param {DACCAggregatesParams} params - The request params
+ */
+export const fetchAggregatesFromDACC = async (
+  client,
+  remoteDoctype,
+  params
+) => {
+  try {
+    const aggregateParams = buildAggregateParams(params)
+    await client
+      .getStackClient()
+      .fetchJSON('POST', `/remote/${remoteDoctype}`, {
+        data: JSON.stringify(aggregateParams),
+        path: 'aggregate'
+      })
+  } catch (error) {
+    log(
+      'error',
+      `Error while fetching aggregates to remote doctype: ${error.message}`
     )
     throw error
   }
--- packages/cozy-client/types/models/dacc.d.ts
+++ packages/cozy-client/types/models/dacc.d.ts
@@ -1,3 +1,11 @@
+export function isCorrectDateFormat(date: any): boolean;
 export function checkMeasureParams(measure: DACCMeasure): void;
 export function sendMeasureToDACC(client: object, remoteDoctype: string, measure: DACCMeasure): Promise<void>;
+export function buildAggregateParams(params: any): {
+    measureName: string;
+    startDate: any;
+    endDate: any;
+};
+export function fetchAggregatesFromDACC(client: object, remoteDoctype: string, params: DACCAggregatesParams): Promise<void>;
 import { DACCMeasure } from "../types";
+import { DACCAggregatesParams } from "../types";
