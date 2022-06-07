fc994a3dbb468130e331e528e7927d1b89f0f2ec
Paul Tran-Van <paul@cozycloud.cc>
2022-06-07 17:06:46 +0200

    docs: Add doc about DACC model

--- packages/cozy-client/types/models/dacc.d.ts
+++ packages/cozy-client/types/models/dacc.d.ts
@@ -1,11 +1,25 @@
-export function isCorrectDateFormat(date: any): boolean;
+export function isCorrectDateFormat(date: string): boolean;
 export function checkMeasureParams(measure: DACCMeasure): void;
-export function sendMeasureToDACC(client: object, remoteDoctype: string, measure: DACCMeasure): Promise<void>;
-export function buildAggregateParams(params: any): {
-    measureName: string;
-    startDate: any;
-    endDate: any;
+export function sendMeasureToDACC(client: CozyClient, remoteDoctype: string, measure: DACCMeasure): Promise<void>;
+export function buildAggregateParams(params: Params): DACCAggregatesParams;
+export function fetchAggregatesFromDACC(client: CozyClient, remoteDoctype: string, params: DACCAggregatesParams): Promise<void>;
+/**
+ * - The unformatted DACC aggregate params
+ */
+export type Params = {
+    /**
+     * - The measure name
+     */
+    measureName?: string;
+    /**
+     * - The measure start date
+     */
+    startDate?: string;
+    /**
+     * - The measure end date
+     */
+    endDate?: string;
 };
-export function fetchAggregatesFromDACC(client: object, remoteDoctype: string, params: DACCAggregatesParams): Promise<void>;
 import { DACCMeasure } from "../types";
+import CozyClient from "../CozyClient";
 import { DACCAggregatesParams } from "../types";
