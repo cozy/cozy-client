cozy/cozy-client

commit 3378b6fe9023682fc716acd04b750afe605c8989

refactor: Move mango index helpers in separate file

packages/cozy-stack-client/src/mangoIndex.js
@@ -1,0 +1,39 @@
+import { transform } from './utils'
+import head from 'lodash/head'
+import get from 'lodash/get'
+import difference from 'lodash/difference'
+import isEqual from 'lodash/difference'
+
+export const getIndexNameFromFields = fields => {
+  return `by_${fields.join('_and_')}`
+}
+
+export const transformSort = sort => {
+  if (!sort || Array.isArray(sort)) {
+    return sort
+  }
+  console.warn(
+    'Passing an object to the "sort" is deprecated, please use an array instead.'
+  )
+  return transform(
+    sort,
+    (acc, order, field) => acc.push({ [field]: order }),
+    []
+  )
+}
+
+/**
+ * Compute fields that should be indexed for a mango
+ * query to work
+ *
+ * @param  {object} options - Mango query options
+ * @returns {Array} - Fields to index
+ */
+export const getIndexFields = ({ selector, sort = [] }) => {
+  return Array.from(
+    new Set([
+      ...sort.map(sortOption => head(Object.keys(sortOption))),
+      ...(selector ? Object.keys(selector) : [])
+    ])
+  )
+}

packages/cozy-stack-client/src/DocumentCollection.js
@@ -12,6 +12,13 @@ import Collection, {
   isIndexNotFoundError,
   isIndexConflictError
 } from './Collection'
+import {
+  getIndexNameFromFields,
+  transformSort,
+  getIndexFields,
+  getMatchingIndex,
+  normalizeDesignDoc
+} from './mangoIndex'
 import * as querystring from './querystring'
 import { FetchError } from './errors'
 
@@ -363,23 +370,16 @@ class DocumentCollection {
     return this.updateAll(docs.map(prepareForDeletion))
   }
 
-  async toMangoOptions(selector, options = {}) {
+  toMangoOptions(selector, options = {}) {
     let { sort, indexedFields } = options
     const { fields, skip = 0, limit, bookmark } = options
 
-    if (sort && !Array.isArray(sort)) {
-      console.warn(
-        'Passing an object to the "sort" is deprecated, please use an array instead.'
-      )
-      sort = transform(
-        sort,
-        (acc, order, field) => acc.push({ [field]: order }),
-        []
-      )
-    }
+    sort = transformSort(sort)
+
     indexedFields = indexedFields
       ? indexedFields
-      : this.getIndexFields({ sort, selector })
+      : getIndexFields({ sort, selector })
+    const indexId = options.indexId || getIndexNameFromFields(indexedFields)
 
     const indexId =
       options.indexId || this.getIndexNameFromFields(indexedFields)

commit 6786769fa9ca5f31cc630cbc3787b5ce79bb9917

fix: Throw error instead of not existing code
At first this should be considered as a Breaking Change,
but the code is broken since
commit 3378b6fe9023682fc716acd04b750afe605c8989
I prefer to not tag it as BC.

packages/cozy-stack-client/src/mangoIndex.js
@@ -1,8 +1,6 @@
-import { transform } from './utils'
 import head from 'lodash/head'
 import get from 'lodash/get'
 import isEqual from 'lodash/isEqual'
-import logger from './logger'
 
 /**
  * @typedef {object} MangoPartialFilter
@@ -75,14 +73,7 @@ export const transformSort = sort => {
   if (!sort || Array.isArray(sort)) {
     return sort
   }
-  logger.warn(
-    'Passing an object to the "sort" is deprecated, please use an array instead.'
-  )
-  return transform(
-    sort,
-    (acc, order, field) => acc.push({ [field]: order }),
-    []
-  )
+  throw Error('Passing an object to the "sort" is not supported')
 }
 
 /**