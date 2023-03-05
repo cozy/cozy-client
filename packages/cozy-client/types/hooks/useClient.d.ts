b946c5208c9696eaac7731e0033b5818abd2bddf
Ldoppea <yannick.chiron@gmail.com>
2023-03-05 17:18:57 +0100

    fix: Correctly type nullable return for `useClient()`
    If no `CozyProvider` is set on the components tree, then `useClient()`
    would return no `client`

--- packages/cozy-client/types/hooks/useClient.d.ts
+++ packages/cozy-client/types/hooks/useClient.d.ts
@@ -2,7 +2,7 @@ export default useClient;
 /**
  * Returns the cozy client from the context
  *
- * @returns {CozyClient} - Current cozy client
+ * @returns {CozyClient|null} - Current cozy client
  */
-declare function useClient(): CozyClient;
+declare function useClient(): CozyClient | null;
 import CozyClient from "../CozyClient";
