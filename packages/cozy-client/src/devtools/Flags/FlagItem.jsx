938bd897edfcfdf6674e9baaf0b5dd2873a5baed
cballevre <celestin.ballevre@protonmail.com>
2023-11-10 13:50:19 +0100

    feat: Improve design with ListItem

--- packages/cozy-client/src/devtools/Flags/FlagItem.jsx
+++ packages/cozy-client/src/devtools/Flags/FlagItem.jsx
@@ -5,23 +5,53 @@ import IconButton from 'cozy-ui/transpiled/react/IconButton'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import PenIcon from 'cozy-ui/transpiled/react/Icons/Pen'
 import TrashIcon from 'cozy-ui/transpiled/react/Icons/Trash'
+import ListItem from 'cozy-ui/transpiled/react/ListItem'
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+import ListItemSecondaryAction from 'cozy-ui/transpiled/react/ListItemSecondaryAction'
+import Checkbox from 'cozy-ui/transpiled/react/Checkbox'
+import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
+import flagUtils from 'cozy-flags'
 
 const FlagItem = ({ flag, onEdit, onTrash }) => {
+  const handleCheckboxChange = e => {
+    flagUtils(flag.name, e.target.checked)
+    location.reload()
+  }
+
   return (
-    <div key={flag.name}>
-      {flag.humanName} :
-      {typeof flag.value === 'object' ? (
-        <ObjectInspector data={flag.value} />
-      ) : (
-        flag.humanValue
-      )}
-      <IconButton size="small" onClick={() => onEdit(flag)}>
-        <Icon icon={PenIcon} />
-      </IconButton>
-      <IconButton size="small" onClick={() => onTrash(flag)}>
-        <Icon icon={TrashIcon} />
-      </IconButton>
-    </div>
+    <ListItem size="small">
+      <ListItemIcon>
+        {flag.type === 'boolean' ? (
+          <Checkbox
+            size="small"
+            checked={flag.value}
+            onChange={handleCheckboxChange}
+          />
+        ) : null}
+      </ListItemIcon>
+      <ListItemText
+        primary={flag.humanName}
+        secondary={
+          flag.type === 'object' ? (
+            <ObjectInspector data={flag.value} />
+          ) : flag.type !== 'boolean' ? (
+            flag.humanValue
+          ) : null
+        }
+      />
+      <ListItemSecondaryAction>
+        <IconButton size="small" onClick={() => onEdit(flag)}>
+          <Icon icon={PenIcon} />
+        </IconButton>
+        <IconButton
+          className="u-ml-1"
+          size="small"
+          onClick={() => onTrash(flag)}
+        >
+          <Icon icon={TrashIcon} />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
   )
 }
 
