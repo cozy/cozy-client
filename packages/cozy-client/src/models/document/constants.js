c7e5bef864378a91d4dacc0b314afd94bc29197e
Alexis G <merkkur39@gmail.com>
2024-03-27 16:34:59 +0100

    refactor: Move all constants for items of themes to a specific file
    We will soon need new constants for adding icons on qualifications.

---
+++ packages/cozy-client/src/models/document/constants.js
@@ -0,0 +1,174 @@
+// #region Qualification labels for each theme
+/**
+ * @type {Array<import("../../types").IdentityLabel>}
+ */
+export const THEME_IDENTITY_LABELS = [
+  'identity_photo',
+  'national_id_card',
+  'passport',
+  'residence_permit',
+  'electoral_card',
+  'family_record_book',
+  'birth_certificate',
+  'driver_license',
+  'citizen_registration_certificate',
+  'note_identity_document',
+  'other_identity_document'
+]
+
+/**
+ * @type {Array<import("../../types").FamilyLabel>}
+ */
+export const THEME_FAMILY_LABELS = [
+  'family_record_book',
+  'birth_certificate',
+  'wedding',
+  'pacs',
+  'single_parent_benefit',
+  'divorce',
+  'large_family_card',
+  'caf',
+  'payment_proof_family_allowance',
+  'note_family_document',
+  'other_family_document',
+  'person_insurance'
+]
+
+/**
+ * @type {Array<import("../../types").WorkStudyLabels>}
+ */
+export const THEME_WORK_STUDY_LABELS = [
+  'diploma',
+  'work_contract',
+  'pay_sheet',
+  'unemployment_benefit',
+  'employment_center_certificate',
+  'pension',
+  'work_disability_recognition',
+  'gradebook',
+  'student_card',
+  'school_attendance_certificate',
+  'school_insurance_certificate',
+  'resume',
+  'motivation_letter',
+  'expense_claim',
+  'note_work_document',
+  'other_work_document'
+]
+
+/**
+ * @type {Array<import("../../types").HealthLabels>}
+ */
+export const THEME_HEALTH_LABELS = [
+  'health_certificate',
+  'health_book',
+  'national_health_insurance_card',
+  'national_health_insurance_right_certificate',
+  'health_insurance_card',
+  'prescription',
+  'health_invoice',
+  'work_disability_recognition',
+  'pregnancy_medical_certificate',
+  'note_health_document',
+  'other_health_document'
+]
+
+/**
+ * @type {Array<import("../../types").HomeLabels>}
+ */
+export const THEME_HOME_LABELS = [
+  'phone_invoice',
+  'isp_invoice',
+  'telecom_invoice',
+  'energy_invoice',
+  'water_invoice',
+  'other_invoice',
+  'work_invoice',
+  'house_sale_agreeement',
+  'building_permit',
+  'technical_diagnostic_record',
+  'unfit_for_habitation_declaration',
+  'lease',
+  'rent_receipt',
+  'accommodation_proof',
+  'house_insurance',
+  'work_quote',
+  'condition_report',
+  'note_house_document',
+  'other_house_document',
+  'real_estate_insurance'
+]
+
+/**
+ * @type {Array<import("../../types").TransportLabels>}
+ */
+export const THEME_TRANSPORT_LABELS = [
+  'driver_license',
+  'vehicle_registration',
+  'car_insurance',
+  'transport_card',
+  'mechanic_invoice',
+  'transport_invoice',
+  'note_transport_document',
+  'other_transport_document'
+]
+
+/**
+ * @type {Array<import("../../types").ActivityLabels>}
+ */
+export const THEME_ACTIVITY_LABELS = [
+  'personal_sporting_licence',
+  'fidelity_card',
+  'library_card',
+  'note_activity_document',
+  'other_activity_document'
+]
+
+/**
+ * @type {Array<import("../../types").FinanceLabels>}
+ */
+export const THEME_FINANCE_LABELS = [
+  'fine',
+  'tax_return',
+  'tax_notice',
+  'tax_timetable',
+  'real_estate_tax',
+  'pay_sheet',
+  'receipt',
+  'single_parent_benefit',
+  'other_tax_document',
+  'bank_details',
+  'bank_statement',
+  'loan_agreement',
+  'payment_proof_family_allowance',
+  'note_finance',
+  'expense_claim',
+  'other_bank_document',
+  'other_revenue'
+]
+
+/**
+ * @type {Array<import("../../types").InvoiceLabels>}
+ */
+export const THEME_INVOICE_LABELS = [
+  'phone_invoice',
+  'isp_invoice',
+  'telecom_invoice',
+  'energy_invoice',
+  'water_invoice',
+  'appliance_invoice',
+  'web_service_invoice',
+  'restaurant_invoice',
+  'work_invoice',
+  'transport_invoice',
+  'health_invoice',
+  'note_invoice',
+  'expense_claim',
+  'other_invoice'
+]
+
+/**
+ * @type {Array<import("../../types").OthersLabels>}
+ */
+export const THEME_OTHERS_LABELS = ['other_administrative_document']
+// #endregion Qualification labels for each theme
