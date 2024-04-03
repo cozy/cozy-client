6ec981426c81b30663f05c7140859913d66dc06b
Théo Poizat <hello@zatteo.com>
2024-04-03 11:53:44 +0200

    test: Refactor ContactsCollection test describe

--- packages/cozy-stack-client/src/ContactsCollection.spec.js
+++ packages/cozy-stack-client/src/ContactsCollection.spec.js
@@ -16,63 +16,66 @@ const stackMyselfResponse = {
 }
 
 describe('ContactsCollection', () => {
-  const setup = () => {
-    const stackClient = new CozyStackClient({})
-    stackClient.fetchJSON = jest
-      .fn()
-      .mockImplementation(async (method, route) => {
-        if (method === 'POST' && route === '/contacts/myself') {
-          return stackMyselfResponse
-        } else if (route.includes('_index')) {
-          return { result: 'exists' }
-        } else {
-          return { docs: [] }
-        }
-      })
-    const col = new ContactsCollection('io.cozy.contacts', stackClient)
-    return { stackClient, col }
-  }
-  beforeEach(() => {
-    jest.spyOn(console, 'warn').mockImplementation(() => {})
-  })
+  describe('find', () => {
+    const setup = () => {
+      const stackClient = new CozyStackClient({})
+      stackClient.fetchJSON = jest
+        .fn()
+        .mockImplementation(async (method, route) => {
+          if (method === 'POST' && route === '/contacts/myself') {
+            return stackMyselfResponse
+          } else if (route.includes('_index')) {
+            return { result: 'exists' }
+          } else {
+            return { docs: [] }
+          }
+        })
+      const col = new ContactsCollection('io.cozy.contacts', stackClient)
+      return { stackClient, col }
+    }
 
-  it('call find route if there is no selector', async () => {
-    const { col, stackClient } = setup()
-    await col.find()
-    expect(stackClient.fetchJSON).toHaveBeenCalledWith(
-      'POST',
-      '/data/io.cozy.contacts/_find',
-      expect.any(Object)
-    )
-  })
-
-  it('should use a special route for the myself contact', async () => {
-    const { col, stackClient } = setup()
-    const resp = await col.find({
-      me: true
+    beforeEach(() => {
+      jest.spyOn(console, 'warn').mockImplementation(() => {})
     })
-    expect(stackClient.fetchJSON).toHaveBeenCalledWith(
-      'POST',
-      '/contacts/myself'
-    )
-    expect(resp.data).toEqual([
-      expect.objectContaining({
-        _id: 'bf91cce0-ef48-0137-2638-543d7eb8149c',
-        id: 'bf91cce0-ef48-0137-2638-543d7eb8149c',
-        fullname: 'Alice'
-      })
-    ])
-  })
 
-  it('should not use a special route for other types of find', async () => {
-    const { col, stackClient } = setup()
-    await col.find({
-      name: 'Alice'
+    it('call find route if there is no selector', async () => {
+      const { col, stackClient } = setup()
+      await col.find()
+      expect(stackClient.fetchJSON).toHaveBeenCalledWith(
+        'POST',
+        '/data/io.cozy.contacts/_find',
+        expect.any(Object)
+      )
     })
-    expect(stackClient.fetchJSON).toHaveBeenCalledWith(
-      'POST',
-      '/data/io.cozy.contacts/_find',
-      expect.objectContaining({ selector: { name: 'Alice' } })
-    )
+
+    it('should use a special route for the myself contact', async () => {
+      const { col, stackClient } = setup()
+      const resp = await col.find({
+        me: true
+      })
+      expect(stackClient.fetchJSON).toHaveBeenCalledWith(
+        'POST',
+        '/contacts/myself'
+      )
+      expect(resp.data).toEqual([
+        expect.objectContaining({
+          _id: 'bf91cce0-ef48-0137-2638-543d7eb8149c',
+          id: 'bf91cce0-ef48-0137-2638-543d7eb8149c',
+          fullname: 'Alice'
+        })
+      ])
+    })
+
+    it('should not use a special route for other types of find', async () => {
+      const { col, stackClient } = setup()
+      await col.find({
+        name: 'Alice'
+      })
+      expect(stackClient.fetchJSON).toHaveBeenCalledWith(
+        'POST',
+        '/data/io.cozy.contacts/_find',
+        expect.objectContaining({ selector: { name: 'Alice' } })
+      )
+    })
   })
 })
