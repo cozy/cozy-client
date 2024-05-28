e6211871262447f746780fab0a0d26b449240a06
Ldoppea <yannick.chiron@gmail.com>
2024-05-28 19:25:59 +0200

    refactor(pouch-link): Make localStorage a class an return async methods
    We want to reduce coupling between CozyPouchLink and the browser's
    local storage
    
    The first step is to groupe existing methods into a PouchLocalStorage
    class, this will allow (in the next commit) to inject a storage adapter
    into the class constructor
    
    Also in some platforms like ReactNative, the localstorage methods are
    asynchronous. So in order to be compatible with all platforms we want
    those methods to become async by default
    
    The big main of making those methods async is that we cannot call them
    from the `PouchManager` constructor. So we introduce a new `.init()`
    method in the `PouchManager` class. This async method now contains all
    the initialization logic and should be called after creating a new
    `PouchManager`
    
    As `PouchManager` is called internally by `CozyPouchLink` and is not
    meant to be a publicly available class, then we don't consider this as
    a breaking change

--- packages/cozy-pouch-link/src/startReplication.spec.js
+++ packages/cozy-pouch-link/src/startReplication.spec.js
@@ -1,12 +1,6 @@
 import { fetchRemoteLastSequence, fetchRemoteInstance } from './remote'
-import { getLastReplicatedDocID } from './localStorage'
 
 import { replicateAllDocs } from './startReplication'
-
-jest.mock('./localStorage', () => ({
-  getLastReplicatedDocID: jest.fn(),
-  persistLastReplicatedDocID: jest.fn()
-}))
 
 jest.mock('./remote', () => ({
   fetchRemoteLastSequence: jest.fn(),
@@ -27,22 +21,27 @@ const generateDocs = nDocs => {
   return docs
 }
 
+const storage = {
+  getLastReplicatedDocID: jest.fn(),
+  persistLastReplicatedDocID: jest.fn()
+}
+
 describe('replication through _all_docs', () => {
   beforeEach(() => {
     fetchRemoteLastSequence.mockResolvedValue('10-xyz')
   })
   it('should replicate all docs', async () => {
-    getLastReplicatedDocID.mockReturnValue(null)
+    storage.getLastReplicatedDocID.mockReturnValue(null)
     const dummyDocs = generateDocs(2)
     fetchRemoteInstance.mockResolvedValue({ rows: dummyDocs })
 
-    const rep = await replicateAllDocs(null, url)
+    const rep = await replicateAllDocs(null, url, undefined, storage)
     const expectedDocs = dummyDocs.map(doc => doc.doc)
     expect(rep).toEqual(expectedDocs)
   })
 
   it('should replicate all docs when it gets more docs than the batch limit', async () => {
-    getLastReplicatedDocID.mockReturnValue(null)
+    storage.getLastReplicatedDocID.mockReturnValue(null)
     const dummyDocs = generateDocs(1002)
     fetchRemoteInstance.mockResolvedValueOnce({
       rows: dummyDocs.slice(0, 1001)
@@ -51,17 +50,17 @@ describe('replication through _all_docs', () => {
       rows: dummyDocs.slice(1000, 1002)
     })
 
-    const rep = await replicateAllDocs(null, url)
+    const rep = await replicateAllDocs(null, url, undefined, storage)
     const expectedDocs = dummyDocs.map(doc => doc.doc)
     expect(rep).toEqual(expectedDocs)
   })
 
   it('should replicate from the last saved doc id', async () => {
-    getLastReplicatedDocID.mockReturnValue('5')
+    storage.getLastReplicatedDocID.mockReturnValue('5')
     const dummyDocs = generateDocs(10)
     fetchRemoteInstance.mockResolvedValue({ rows: dummyDocs.slice(5, 11) })
 
-    const rep = await replicateAllDocs(null, url)
+    const rep = await replicateAllDocs(null, url, undefined, storage)
 
     const calledUrl = new URL(`${url}/_all_docs`)
     expect(fetchRemoteInstance).toHaveBeenCalledWith(calledUrl, {
