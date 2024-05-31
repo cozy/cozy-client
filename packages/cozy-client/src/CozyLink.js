0ed72152f0234f3865089173e688f65bcb9185b6
Ldoppea <yannick.chiron@gmail.com>
2024-05-31 16:41:20 +0200

    feat: Persist documents to local Pouch when they have no `rev`
    When a PouchLink is configured, the synchronization process would
    retrieve only documents that exists on the remote database
    
    Those documents would be served as usual when a `.query()` is made
    using the PouchLink
    
    Unfortunately, not all `.query()` correspond to a document that exists
    on the remote database. With the collection mechanism, some documents
    are retrieved using an alternative endpoint and then the cozy-stack
    generate the document on-demand
    
    For example this is the case for `io.cozy.settings.flags`,
    `io.cozy.settings.context` and `io.cozy.settings.disk-usage`
    
    Because those documents does not actually exist on the remote database,
    we cannot sync them using the PouchDB sync mechanism and so they won't
    be available when the device is offline
    
    To fix this, we chose to manually insert those document in the local
    PouchDB when we detect that a `.query()` call retrieve them from the
    cozy-stack
    
    The easiest way we found to detect them is to check for the existence
    of `meta.rev` object in the `.query()` result. If no `rev` is present,
    this means that the documents does not come from the remote database
    but has been dynamically generated by the cozy-stack
    
    Those documents are inserted into existing PouchBD with the
    `cozyLocalOnly` attributes, so we know that they should not be
    replicated to the remote database on next sync
    
    In order to make the persist mechanism possible, we implemented a new
    `persistData()` method to the CozyLink interface
    
    The impact is that every `Link` should now implement the
    `persistData()` interface that should persist the data if it is able to
    do it, or to forward the action otherwise
    
    If no `Link` process the action, then the final "default" method will
    do nothing (contrary to the default for `request()` that would throw an
    error)
    
    BREAKING CHANGE: CozyClient's links should now implement a
    `persistData()` method that should persist the given data if it is able
    to do it, or to forward the action to the next `Link` otherwise

--- packages/cozy-client/src/CozyLink.js
+++ packages/cozy-client/src/CozyLink.js
@@ -1,19 +1,27 @@
 export default class CozyLink {
-  constructor(requestHandler) {
+  constructor(requestHandler, persistHandler) {
     if (typeof requestHandler === 'function') {
       this.request = requestHandler
+    }
+
+    if (typeof persistHandler === 'function') {
+      this.persistData = persistHandler
     }
   }
 
   request(operation, result, forward) {
     throw new Error('request is not implemented')
   }
+
+  persistData(data, forward) {
+    throw new Error('persistData is not implemented')
+  }
 }
 
 const toLink = handler =>
   typeof handler === 'function' ? new CozyLink(handler) : handler
 
-const defaultLinkHandler = (operation, result) => {
+const defaultLinkRequestHandler = (operation, result) => {
   if (result) return result
   else if (operation.execute) return operation.execute()
   else
@@ -22,14 +30,32 @@ const defaultLinkHandler = (operation, result) => {
     )
 }
 
+const defaultLinkPersistHandler = (operation, result) => {
+  // Do nothing
+}
+
+const defaultLinkHandler = new CozyLink(
+  defaultLinkRequestHandler,
+  defaultLinkPersistHandler
+)
+
 export const chain = links =>
   [...links, defaultLinkHandler].map(toLink).reduce(concat)
 
 const concat = (firstLink, nextLink) => {
-  return new CozyLink((operation, result, forward) => {
+  const requestHandler = (operation, result, forward) => {
     const nextForward = (op, res) => {
       return nextLink.request(op, res, forward)
     }
     return firstLink.request(operation, result, nextForward)
-  })
+  }
+
+  const persistHandler = (data, forward) => {
+    const nextForward = d => {
+      return nextLink.persistData(d, forward)
+    }
+    return firstLink.persistData(data, nextForward)
+  }
+
+  return new CozyLink(requestHandler, persistHandler)
 }
