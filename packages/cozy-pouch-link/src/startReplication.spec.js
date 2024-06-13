b20bd44ed80d4618c747b5940850aa9bf3db1b94
Ldoppea <yannick.chiron@gmail.com>
2024-06-13 12:24:43 +0200

    refactor: Nest startReplication tests in corresponding describe block

--- packages/cozy-pouch-link/src/startReplication.spec.js
+++ packages/cozy-pouch-link/src/startReplication.spec.js
@@ -26,49 +26,52 @@ const storage = {
   persistLastReplicatedDocID: jest.fn()
 }
 
-describe('replication through _all_docs', () => {
+describe('startReplication', () => {
   beforeEach(() => {
     fetchRemoteLastSequence.mockResolvedValue('10-xyz')
   })
-  it('should replicate all docs', async () => {
-    storage.getLastReplicatedDocID.mockReturnValue(null)
-    const dummyDocs = generateDocs(2)
-    fetchRemoteInstance.mockResolvedValue({ rows: dummyDocs })
 
-    const rep = await replicateAllDocs(null, url, undefined, storage)
-    const expectedDocs = dummyDocs.map(doc => doc.doc)
-    expect(rep).toEqual(expectedDocs)
-  })
+  describe('replication through _all_docs', () => {
+    it('should replicate all docs', async () => {
+      storage.getLastReplicatedDocID.mockReturnValue(null)
+      const dummyDocs = generateDocs(2)
+      fetchRemoteInstance.mockResolvedValue({ rows: dummyDocs })
 
-  it('should replicate all docs when it gets more docs than the batch limit', async () => {
-    storage.getLastReplicatedDocID.mockReturnValue(null)
-    const dummyDocs = generateDocs(1002)
-    fetchRemoteInstance.mockResolvedValueOnce({
-      rows: dummyDocs.slice(0, 1001)
-    })
-    fetchRemoteInstance.mockResolvedValueOnce({
-      rows: dummyDocs.slice(1000, 1002)
+      const rep = await replicateAllDocs(null, url, undefined, storage)
+      const expectedDocs = dummyDocs.map(doc => doc.doc)
+      expect(rep).toEqual(expectedDocs)
     })
 
-    const rep = await replicateAllDocs(null, url, undefined, storage)
-    const expectedDocs = dummyDocs.map(doc => doc.doc)
-    expect(rep).toEqual(expectedDocs)
-  })
+    it('should replicate all docs when it gets more docs than the batch limit', async () => {
+      storage.getLastReplicatedDocID.mockReturnValue(null)
+      const dummyDocs = generateDocs(1002)
+      fetchRemoteInstance.mockResolvedValueOnce({
+        rows: dummyDocs.slice(0, 1001)
+      })
+      fetchRemoteInstance.mockResolvedValueOnce({
+        rows: dummyDocs.slice(1000, 1002)
+      })
 
-  it('should replicate from the last saved doc id', async () => {
-    storage.getLastReplicatedDocID.mockReturnValue('5')
-    const dummyDocs = generateDocs(10)
-    fetchRemoteInstance.mockResolvedValue({ rows: dummyDocs.slice(5, 11) })
-
-    const rep = await replicateAllDocs(null, url, undefined, storage)
-
-    const calledUrl = new URL(`${url}/_all_docs`)
-    expect(fetchRemoteInstance).toHaveBeenCalledWith(calledUrl, {
-      include_docs: true,
-      limit: 1000,
-      startkey_docid: '5'
+      const rep = await replicateAllDocs(null, url, undefined, storage)
+      const expectedDocs = dummyDocs.map(doc => doc.doc)
+      expect(rep).toEqual(expectedDocs)
     })
-    const expectedDocs = dummyDocs.map(doc => doc.doc).slice(6, 11)
-    expect(rep).toEqual(expectedDocs)
+
+    it('should replicate from the last saved doc id', async () => {
+      storage.getLastReplicatedDocID.mockReturnValue('5')
+      const dummyDocs = generateDocs(10)
+      fetchRemoteInstance.mockResolvedValue({ rows: dummyDocs.slice(5, 11) })
+
+      const rep = await replicateAllDocs(null, url, undefined, storage)
+
+      const calledUrl = new URL(`${url}/_all_docs`)
+      expect(fetchRemoteInstance).toHaveBeenCalledWith(calledUrl, {
+        include_docs: true,
+        limit: 1000,
+        startkey_docid: '5'
+      })
+      const expectedDocs = dummyDocs.map(doc => doc.doc).slice(6, 11)
+      expect(rep).toEqual(expectedDocs)
+    })
   })
 })
