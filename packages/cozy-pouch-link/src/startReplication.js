b5fc2a9d4b64e521798970e96f42f8cd8daabc8d
Ldoppea <yannick.chiron@gmail.com>
2024-06-13 13:01:59 +0200

    fix: Make cancelling replication work
    The `startReplication` promise is expected to be cancelable
    
    This is done by storing the `pouch.replicate` call into a `replication`
    variable, so we can use it to call the `cancel()` method
    
    With current implementation, the `replication` variable exists in the
    `startReplication` scope but also in the child Promise scope, so the
    parent variable is hidden by the child one, which prevent to access it
    from the parent scope
    
    Deleting the child variable is enough to fix the problem

--- packages/cozy-pouch-link/src/startReplication.js
+++ packages/cozy-pouch-link/src/startReplication.js
@@ -69,7 +69,7 @@ export const startReplication = (
         }
       }
     }
-    let replication
+
     if (initialReplication && strategy !== 'toRemote') {
       ;(async () => {
         // For the first remote->local replication, we manually replicate all docs
