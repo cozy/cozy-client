e2f492906861e01f394b33890d6b51113c9516cb
Ldoppea <yannick.chiron@gmail.com>
2024-07-16 16:54:39 +0200

    fix: Clean types folder before running `types` script
    When calling `yarn types` all cozy-client's types are generated inside
    of the `cozy-client/types` folder
    
    However, there is no mechanism to remove types when their corresponding
    class is deleted
    
    To fix this, we want to clean the types folder before regenerating
    types so we ensure no fantom type would persist

--- packages/cozy-client/types/queries/referencedBy.d.ts
+++
@@ -1,7 +0,0 @@
-export function isReferencedBy(file: IOCozyFile, referencedBy: Doctype): boolean;
-export function isReferencedById(file: IOCozyFile, referencedBy: Doctype, referencedId: string): boolean;
-export function getReferencedBy(file: IOCozyFile, referencedBy: Doctype): Reference[];
-export function getReferencedById(file: IOCozyFile, referencedBy: Doctype, referencedId: string): Reference[];
-import { IOCozyFile } from "../types";
-import { Doctype } from "../types";
-import { Reference } from "../types";
