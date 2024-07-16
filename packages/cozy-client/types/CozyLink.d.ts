9cc04e09be07370d4fd811a92298ff16b0a3e5e7
Ldoppea <yannick.chiron@gmail.com>
2024-07-16 17:36:46 +0200

    docs: Update types and documentation

--- packages/cozy-client/types/CozyLink.d.ts
+++ packages/cozy-client/types/CozyLink.d.ts
@@ -1,5 +1,6 @@
 export default class CozyLink {
-    constructor(requestHandler: any);
+    constructor(requestHandler: any, persistHandler: any);
     request(operation: any, result: any, forward: any): void;
+    persistData(data: any, forward: any): void;
 }
 export function chain(links: any): any;
