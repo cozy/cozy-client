dee724506fc57eb79d8b6a9104c4a7f6f9f0643b
Ldoppea <yannick.chiron@gmail.com>
2024-07-26 15:23:50 +0200

    docs: Update types and documentation

--- packages/cozy-pouch-link/types/remote.d.ts
+++ packages/cozy-pouch-link/types/remote.d.ts
@@ -1,2 +1,6 @@
+export const DATABASE_NOT_FOUND_ERROR: "Database does not exist";
+export const DATABASE_RESERVED_DOCTYPE_ERROR: "Reserved doctype";
+export function isDatabaseNotFoundError(error: any): boolean;
+export function isDatabaseUnradableError(error: any): boolean;
 export function fetchRemoteInstance(url: URL, params?: object): Promise<object>;
 export function fetchRemoteLastSequence(baseUrl: string): Promise<string>;
