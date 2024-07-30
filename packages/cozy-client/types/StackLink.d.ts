9054503f9ca2d84e5555dee307df3388c331ea43
Ldoppea <yannick.chiron@gmail.com>
2024-07-30 18:43:34 +0200

    docs: Update types and documentation

--- packages/cozy-client/types/StackLink.d.ts
+++ packages/cozy-client/types/StackLink.d.ts
@@ -2,20 +2,19 @@ export function transformBulkDocsResponse(bulkResponse: import("./types").CouchD
     data: import("./types").CozyClientDocument[];
 };
 /**
+ * @typedef {object} StackLinkOptions
+ * @property {object} [stackClient] - A StackClient
+ * @property {object} [client] - A StackClient (deprecated)
+ * @property {import('cozy-pouch-link/dist/types').LinkPlatform} [platform] - Platform specific adapters and methods
+ */
+/**
  * Transfers queries and mutations to a remote stack
  */
 export default class StackLink extends CozyLink {
     /**
-     * @param {object} [options] - Options
-     * @param  {object} [options.stackClient] - A StackClient
-     * @param  {object} [options.client] - A StackClient (deprecated)
-     * @param {import('cozy-pouch-link/dist/types').LinkPlatform} [options.platform] Platform specific adapters and methods
+     * @param {StackLinkOptions} [options] - Options
      */
-    constructor({ client, stackClient, platform }?: {
-        stackClient: object;
-        client: object;
-        platform: import('cozy-pouch-link/dist/types').LinkPlatform;
-    });
+    constructor({ client, stackClient, platform }?: StackLinkOptions);
     stackClient: any;
     isOnline: any;
     registerClient(client: any): void;
@@ -28,5 +27,19 @@ export default class StackLink extends CozyLink {
     executeQuery(query: QueryDefinition): Promise<import("./types").ClientResponse>;
     executeMutation(mutation: any, result: any, forward: any): Promise<any>;
 }
+export type StackLinkOptions = {
+    /**
+     * - A StackClient
+     */
+    stackClient?: object;
+    /**
+     * - A StackClient (deprecated)
+     */
+    client?: object;
+    /**
+     * - Platform specific adapters and methods
+     */
+    platform?: any;
+};
 import CozyLink from "./CozyLink";
 import { QueryDefinition } from "./queries/dsl";
