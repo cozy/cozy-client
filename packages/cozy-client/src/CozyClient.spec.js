fa1cef9f02f6c7dbcec663d2195b31aa8af28f22
Ldoppea <yannick.chiron@gmail.com>
2024-08-13 16:44:44 +0200

    feat: Add check to `_rev` attribute when persisting virtual documents
    In previous commit we added a check to `meta.rev` attribute in order to
    discriminate documents that exist in remote CouchDB from the ones that
    don't
    
    This approach would work only for documents handled by the cozy-stack
    using JSON-API
    
    But some CouchDB documents are served directly through the `data` route
    and are not wrapped into JSON-API
    
    When this is the case, they have a `_rev` attribute instead of a
    `meta.rev` one
    
    So we want to check for both forms before persisting virtual documents

--- packages/cozy-client/src/CozyClient.spec.js
+++ packages/cozy-client/src/CozyClient.spec.js
@@ -1403,7 +1403,7 @@ describe('CozyClient', () => {
       expect(executeQueryFromState).toHaveBeenCalledTimes(1)
     })
 
-    it('should persist virtual document when no meta.rev', async () => {
+    it('should persist virtual document when no meta.rev nor _rev', async () => {
       jest.spyOn(client, 'requestQuery')
       requestHandler.mockResolvedValue({
         data: {
@@ -1421,7 +1421,37 @@ describe('CozyClient', () => {
       )
     })
 
-    it('should persist array of virtual documents when no meta.rev', async () => {
+    it('should not persist virtual document when meta.rev', async () => {
+      jest.spyOn(client, 'requestQuery')
+      requestHandler.mockResolvedValue({
+        data: {
+          _id: 'some_id',
+          meta: {
+            rev: 'SOME_REV'
+          }
+        }
+      })
+
+      await client.query(query, { as: 'allTodos' })
+
+      expect(persistHandler).not.toHaveBeenCalled()
+    })
+
+    it('should not persist virtual document when _rev', async () => {
+      jest.spyOn(client, 'requestQuery')
+      requestHandler.mockResolvedValue({
+        data: {
+          _id: 'some_id',
+          _rev: 'SOME_REV'
+        }
+      })
+
+      await client.query(query, { as: 'allTodos' })
+
+      expect(persistHandler).not.toHaveBeenCalled()
+    })
+
+    it('should persist array of virtual documents when no meta.rev nor _rev', async () => {
       jest.spyOn(client, 'requestQuery')
       requestHandler.mockResolvedValue({
         data: [
