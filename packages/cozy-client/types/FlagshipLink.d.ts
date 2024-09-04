1a9a6ce512266244088f1511ac20b1d7b2d63f9b
Ldoppea <yannick.chiron@gmail.com>
2024-09-04 18:11:43 +0200

    docs: Update types and documentation

--- packages/cozy-client/types/FlagshipLink.d.ts
+++ packages/cozy-client/types/FlagshipLink.d.ts
@@ -1,16 +1,11 @@
 export default class FlagshipLink extends CozyLink {
     /**
      * @param {object} [options] - Options
-     * @param  {object} [options.stackClient] - A StackClient
-     * @param  {object} [options.client] - A StackClient (deprecated)
      * @param  {import('cozy-intent').WebviewService} [options.webviewIntent] - The webview's intent reference
      */
-    constructor({ client, stackClient, webviewIntent }?: {
-        stackClient: object;
-        client: object;
+    constructor({ webviewIntent }?: {
         webviewIntent: import('cozy-intent').WebviewService;
     });
-    stackClient: any;
     webviewIntent: import("cozy-intent").WebviewService;
     registerClient(client: any): void;
     reset(): void;
