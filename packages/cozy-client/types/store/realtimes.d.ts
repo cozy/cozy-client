5195583217f86132c8bd3f3c42b998262fc6f7b1
Ldoppea <yannick.chiron@gmail.com>
2024-09-19 15:47:04 +0200

    fix: Remove realtimes.d.ts
    `realtime.js` has been renamed to `realtime.js` but the old typing file
    has not been removed
    
    Related commit: 4aa9ab0d091a111efebf963278ac7a8e208ded7e

--- packages/cozy-client/types/store/realtimes.d.ts
+++
@@ -1,3 +0,0 @@
-export function dispatchCreate(client: object, doctype: import("../types").Doctype, couchDBDoc: import("../types").CouchDBDocument): void;
-export function dispatchUpdate(client: object, doctype: import("../types").Doctype, couchDBDoc: import("../types").CouchDBDocument): void;
-export function dispatchDelete(client: object, doctype: import("../types").Doctype, couchDBDoc: import("../types").CouchDBDocument): void;
