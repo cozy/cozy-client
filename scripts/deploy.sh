c2cf5d6f78151552bbdbb1ba7f580b3d8256a344
cballevre <celestin.ballevre@protonmail.com>
2024-11-15 16:18:04 +0100

    chore: Remove Travis

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-git checkout master
-git remote set-url origin https://cozy-bot:$GITHUB_TOKEN@github.com/cozy/cozy-client.git
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
-env GH_TOKEN="$GITHUB_TOKEN" yarn lerna publish --yes
