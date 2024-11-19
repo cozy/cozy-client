be015ca926a02dba4bc3ccd49b3b9927f4ed1d62
Théo Poizat <hello@zatteo.com>
2024-11-19 16:39:48 +0100

    test: Remove enzyme package and config and update testing-library
    - Remove enzyme
    - Update testing-library
    - Update testing config to better support testing-library

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,7 @@
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import '@testing-library/jest-dom'
 import fetch from 'whatwg-fetch'
 import { setupConsoleToThrow } from './packages/cozy-client/src/__tests__/console'
 
 setupConsoleToThrow()
 
 global.fetch = fetch
-
-Enzyme.configure({ adapter: new Adapter() })
--- packages/cozy-client/src/__tests__/setup.js
+++ packages/cozy-client/src/__tests__/setup.js
@@ -1,7 +1,3 @@
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
 import fetch from 'whatwg-fetch'
 
 global.fetch = fetch
-
-Enzyme.configure({ adapter: new Adapter() })
