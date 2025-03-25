11fd5d498654b2a99b55649b145447585e0082ad
Paul Tran-Van <paul.tranvan@gmail.com>
2025-03-25 14:46:07 +0100

    fix: Simple query example

--- packages/cozy-client/examples/simple-test-query.js
+++ packages/cozy-client/examples/simple-test-query.js
@@ -1,33 +1,41 @@
-const { createClientInteractive, Q } = require('../dist/index.node')
+const CozyClient = require('../dist/index.node')
 const { ArgumentParser } = require('argparse')
+const { createClientInteractive, Q } = require('../dist/index.node')
 
 global.fetch = require('node-fetch')
 
-// Change those 2 variables accordingly to your needs
 const DOCTYPE = 'io.cozy.files'
 const QUERY = Q(DOCTYPE)
   .where({
     'cozyMetadata.createdByApp': 'drive'
   })
   .indexFields(['cozyMetadata.createdByApp'])
-  .partialIndex({
-    restore_path: { $exists: false },
-    type: 'directory'
-  })
-  .limit(100)
+  .limitBy(100)
 
 const main = async () => {
   const parser = new ArgumentParser()
   parser.addArgument('--url', { defaultValue: 'http://cozy.localhost:8080' })
+  parser.addArgument('--token')
+
   const args = parser.parseArgs()
 
-  const client = await createClientInteractive({
-    scope: [DOCTYPE],
-    uri: args.url,
-    oauth: {
-      softwareID: 'io.cozy.client.cli'
-    }
-  })
+  let client
+  if (args.token) {
+    // You can generate token with `cozy-stack instances token-cli cozy.localhost:8080 io.cozy.files`
+    client = new CozyClient({
+      uri: args.url,
+      token: args.token
+    })
+  } else {
+    console.log('No token provided, lets do OAuth')
+    client = await createClientInteractive({
+      scope: [DOCTYPE],
+      uri: args.url,
+      oauth: {
+        softwareID: 'io.cozy.client.cli'
+      }
+    })
+  }
 
   const resp = await client.query(QUERY)
   console.log(resp)
