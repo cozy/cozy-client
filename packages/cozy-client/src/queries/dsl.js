d63f6d620740e8d50efd2439080cc864736baf31
Théo Poizat <hello@zatteo.com>
2025-07-17 11:10:43 +0200

    feat: Rename dsl method to setSharingId
    dsl method was called sharingId and QueryDefinition
     class attribute was also called sharingId.

--- packages/cozy-client/src/queries/dsl.js
+++ packages/cozy-client/src/queries/dsl.js
@@ -366,7 +366,7 @@ class QueryDefinition {
    * @param {string} id - The sharing doc id
    * @returns {QueryDefinition}  The QueryDefinition object.
    */
-  sharingId(id) {
+  setSharingId(id) {
     return new QueryDefinition({ ...this.toDefinition(), sharingId: id })
   }
 
