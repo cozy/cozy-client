ac5fb4787115caaa742b50150d8cbe1d2e2c24e8
doubleface <christophe@cozycloud.cc>
2025-10-01 15:45:47 +0200

    refactor(cozy-pouch-link): Simplify replication sequence handling
    - Removed unnecessary fetching of the last remote sequence during initial replication.
    - Save the last sequence on replication completion

--- packages/cozy-pouch-link/src/replicateOnce.js
+++ packages/cozy-pouch-link/src/replicateOnce.js
@@ -5,11 +5,7 @@ import startsWith from 'lodash/startsWith'
 import zip from 'lodash/zip'
 
 import logger from './logger'
-import {
-  fetchRemoteLastSequence,
-  isDatabaseNotFoundError,
-  isDatabaseUnradableError
-} from './remote'
+import { isDatabaseNotFoundError, isDatabaseUnradableError } from './remote'
 import { startReplication } from './startReplication'
 import { allSettled, getDoctypeFromDatabaseName } from './utils'
 
@@ -48,15 +44,8 @@ export const replicateOnce = async pouchManager => {
       const replicationFilter = doc => {
         return !startsWith(doc._id, '_design')
       }
-      let seq = ''
-      if (initialReplication) {
-        // Before the first replication, get the last remote sequence,
-        // which will be used as a checkpoint for the next replication
-        const lastSeq = await fetchRemoteLastSequence(getReplicationURL())
-        await pouchManager.storage.persistDoctypeLastSequence(doctype, lastSeq)
-      } else {
-        seq = await pouchManager.storage.getDoctypeLastSequence(doctype)
-      }
+      const seq =
+        (await pouchManager.storage.getDoctypeLastSequence(doctype)) || ''
 
       replicationOptions.initialReplication = initialReplication
       replicationOptions.filter = replicationFilter
--- packages/cozy-pouch-link/types/PouchManager.d.ts
+++ packages/cozy-pouch-link/types/PouchManager.d.ts
@@ -94,7 +94,20 @@ declare class PouchManager {
     checkToWarmupDoctype(doctype: any, replicationOptions: any): void;
     areQueriesWarmedUp(doctype: any, queries: any): Promise<any>;
     clearWarmedUpQueries(): Promise<void>;
+    /**
+     * Adds a new doctype to the list of managed doctypes, sets its replication options,
+     * creates a new PouchDB instance for it, and sets up the query engine.
+     *
+     * @param {string} doctype - The name of the doctype to add.
+     * @param {Object} options - The replication options for the doctype.
+     */
     addDoctype(doctype: string, options: any): void;
+    /**
+     * Removes a doctype from the list of managed doctypes, deletes its replication options,
+     * destroys its PouchDB instance, and removes it from the pouches.
+     *
+     * @param {string} doctype - The name of the doctype to remove.
+     */
     removeDoctype(doctype: string): void;
 }
 import { PouchLocalStorage } from "./localStorage";
